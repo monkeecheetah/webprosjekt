b95708d70af4a4999bff510b5ac66a09fc7e2c2a
chrban <mac@dhcp-158-39-197-12.wlan.hioa.no>
2013-10-31 12:19:33 +0100

    Lagt til kaffhelse
    listeform med kilderef

---
+++ www/pages/kaffehelse.php
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+<h1>Kaffehelse</h1>
+
+<q><p>“Dersom du pøser på med tre til fire espresso rett før trening, kan dette bidra til mindre stølhet”<br>
+- Lise von Krogh, ernæringsfysiolog</p></q>
+
+<ul>
+	<li>Drikker du minst seks kopper eller mer, reduserer du risikoen for prostatakreft med 20% (ifølge denne <a href="http://jnci.oxfordjournals.org/content/103/11/876">studien</a>).</li>
+	<li>Kaffedrikkere utvikler demens eller alzheimer senere i livet, enn de som ikke drikker det.<a href="">http://iospress.metapress.com/content/d885346618q57103/?genre=article&issn=1387-2877&volume=16&issue=1&spage=85</a>(kilde)</li>
+	<li>Lukten av kaffe er bevist å redusere stress som følge av sønmangel. <a href="Denne studien viser: http://pubs.acs.org/doi/abs/10.1021/jf8001137"></a>(kilde)</li>
+	<li>Kvinner som drikker mer enn fire kopper daglig, reduserer risikoen for å få livmorkreft med 25%.<a href="http://cancerres.aacrjournals.org/content/72/13/3282">(kilde)</a></li>
+	<li>Trener du? koffein øker ytelsesevnen under trening med over 10 prsent! <a href="http://onlinelibrary.wiley.com/journal/10.1111/(ISSN)1600-0838">(kilde)</a></li>
+	<li>Drikker du kaffe dalglig reduseres reisikoen for å utvikle den mest vanlige formen av hudkreft.<a href="http://cancerres.aacrjournals.org/content/72/13/3282 "></a>(kilde)</li>
+	<li>Jo mer kaffe du drikker, jo mindre er sannsynligheten for å pådra Parkinson sykdom.</li>
+	<li>Kaffedrikkere som drikker 3-5 kopper har bedre logisk forståelse og scorer høyere på IQ tester og kortidsminne studier.</li>
+	<li>Jo mer kaffe du drikker, jo mindre er sannsynligheten for å utvikle gikt.</li>
+	<li>Koffein hjelper med å stimulere galleblærefunksjonen og kan forebygge gallesteiner.<a href="http://www.hsph.harvard.edu/"></a>Rest kilde</li>
+</ul>
+=======
+<h1>Kaffehelse</h1>
+>>>>>>> ead6c82e121d1109e324d8abfaa43cff2dadbe5f
