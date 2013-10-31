9abc6357adaecc1e009d3b0d3772f98322df4ed3
kristofferosen <kristofferosen@gmail.com>
2013-10-31 12:44:57 +0100

    ryddet, ikke kortet ned så mye

--- www/pages/tilberedning.php
+++ www/pages/tilberedning.php
@@ -1,6 +1,13 @@
 <h1>Tilbredning</h1>
-<p>Det finnes mange veier til en god kopp kaffe. Gode råvarer er alltid viktigst men<br>
+<p>Det finnes mange veier til en god kopp kaffe. <strong>Gode råvarer</strong> er alltid viktigst men<br>
  kaffenerder sverger til forskjellige mange tilbredningsmetoder for å lage kaffen de liker best</p>
+
+ <h2>Råvarer</h2>
+ 	<p>Bra bønner er det viktigste! Det er gøy å kjøpe bønner og kverne dem selv!<br>
+ 	   Det finnes både elektriske kverner og håndkverner. Håndkverner er koselige og <br>
+ 	   det lukter supergodt i hele rommet når man kverner, men man går veldig fort lei<br>
+ 	   av å kverne for hånd hver gang. Dessuten koster gjerne en håndkvern mer enn en elektrisk en.
+ 	</p>
 
  <h2>Kaffe med papirfilter</h2>
 	<h3>Kaffetrakter</h3>
@@ -9,41 +16,85 @@
 	 	   Et par tips for litt mer spennende trakterkaffe:</p>
 	 	<ul>
 	 		<li>Kjøp hele bønner og kvern selv</li>
-	 		<li>Pass på å bruke riktig mengde kaffe<br>6-87kaffesjeer pr. liter </li>
+	 		<li>Pass på å bruke riktig mengde kaffe<br>6-7kaffesjeer pr. liter </li>
 	 		<li>Ikke la kolben stå på varmeplaten for lenge!</li>
 	 		<li>Hell på termokanne</li>
 	 	</ul>
 
 
-	<h3>Manuelt håndbrygg</h3>
-	 	<p>Hvis man vil ha litt mer kontroll over filterbrygging kan man bruke de mer manuelle teknikkersom inngår i håndbrygging. Håndbrygging er et samlebegrep på en del forskjellige måter å lage kaffe på. Det som er mest i vinden for tiden er å bruke en filterholder,(lignende de som brukes på en vanlig kaffetrakter) med spesielt tilpassede filtre, som man plasserer på kaffekoppen og dermed brygger rett i koppen. Dette er som sagt en metode som er veldig i vinden for tiden, og på internett finnes det utallige kaffenerder som har sin egen måte å håndbrygge på, noe som gjør håndbrygging vanskelig å forklare. Hovedpoenget er uansett at du som brygger har større kontroll over prosessen og kan lettere kontrollere hvilke kvaliteter kaffen skal ha.</p>
-	
+	<h3>Brygg manuelt</h3>
+	 	<p>Hvis man vil ha litt mer kontroll over filterbrygging kan man<br>
+	 	   bruke de mer manuelle teknikkersom inngår i <strong>håndbrygging</strong>.<br>
+	 	   Det som er mest i vinden for tiden er å bruke en<br>
+	 	   filterholder,(lignende de som brukes på en vanlig kaffetrakter)<br>
+	 	   med spesielt tilpassede filtre, som man plasserer på kaffekoppen<br>
+	 	   og dermed brygger rett i koppen.<br><br>
+
+	 	   Dette er som sagt en metode som er veldig i vinden for tiden, og<br>
+	 	   på internett finnes det utallige kaffenerder som har sin egen måte<br>
+	 	   å håndbrygge på, noe som gjør håndbrygging vanskelig å forklare. 
+
+	 	   Hovedpoenget er uansett at du som brygger har større <Strong>kontroll</strong><br>
+	 	   over prosessen og kan lettere kontrollere hvilke kvaliteter kaffen skal ha.
+	 	</p>
+
 
 	<h3>Aero-press</h3>
 		<p>jeg blir lei av ordspill som “den heteste kaffetrenden” og “hotte kaffetrender” og skriver om dette når jeg har roet meg, hehe</p>
 
 
 <h2>Prezzkanne</h2>
- 	<p>Presskanne er en rask og enkel måte å lage mye og fyldig kaffe på. Du kan bruke mange forskjellige typer kaffe når du bruker presskanne. Både filter- og kokmalt funker. Du kan også kjøpe kaffe som er kvernet spesielt for presskanne. Den beste veien til god presskaffe er likevel å kjøpe hele bønner og kverne dem selv. Stempelet i en presskanne har et filter av tynne metalltråder som ikke er like fint som et vanlig papir, så skal du kverne bønnene selv, kvern de grovt så minst mulig grut kommer ut i kaffen du skal drikke.
+	<div id="pressinfo">
 
-		En kanne kan du lage lage enkelt, ved å hive oppi en fornuftig mengde kaffe, kokende vann og presse ned etter et par minutter. Hvis du vi eksprimentere litt mer med smaken kan du være litt mer nøye. Kaffenerder og barristaer har laget kaffe i presskanne i mangemange år og har funnet ut hva som funker best. Her er noen tips:</p>
-	<ul>
-		<li>Skal du lage en hel presskanne, som vanligvis er en liter, bør du bruke rundt 60 gram bønner. </li>
-		<li>Kannen burde du forvarme med varmt vann før du har kaffen i.</li>
-		<li>Vannet du bruker skal ikke koke, men være rundt 90-96 grader</li>
-		<li>Hell i vannet i to runder. Fyll kannen halveis opp først og rør rundt, før du heller i resten av vannet.</li>
-		<li>La kaffe kose seg i ganske nøyaktig 4 min og press til slutt ned stempelet.</li>
-		<li>Drikk med en gang!</li>
+	 	<p>Presskanne er en rask og enkel måte å lage mye og fyldig kaffe på.<br>
+	 	   Du kan bruke mange forskjellige typer kaffe i en presskanne.<br>
+	 	   Både <em>filter</em>- og <em>kokmalt</em> funker. Du kan også kjøpe<br>
+	 	   kaffe som er kvernet spesielt for presskanne. Den beste veien til god<br>
+	 	   presskaffe er likevel å kjøpe hele bønner og kverne dem selv. <br>
+	 	   Kvern bønnene grovt, så de ikke slipper igjennom metallfilteret<br>
+	 	</p>
+ 	</div>
 
-	</ul>
+ 	<div id="presstips">
+		<p>En kanne kan du lage lage enkelt, ved å hive oppi en "fornuftig" mengde kaffe,<br>
+		   kokende vann og presse ned etter et par minutter. Hvis du vi eksprimentere<br>
+		   litt mer med smaken kan du være litt mer nøye. Kaffenerder og barristaer har<br>
+		   laget kaffe i presskanne i mangemange år og har funnet ut hva som funker best.<br>
+		   Her er noen tips:
+		</p>
+		<ul>
+			<li>Skal du lage en hel presskanne, som vanligvis er en liter, bør du bruke rundt 60 gram bønner. </li>
+			<li>Kannen burde du forvarme med varmt vann før du har kaffen i.</li>
+			<li>Vannet du bruker skal ikke koke, men være rundt 90-96 grader</li>
+			<li>Hell i vannet i to runder. Fyll kannen halveis opp først og rør rundt, før du heller i resten av vannet.</li>
+			<li>La kaffe kose seg i ganske nøyaktig 4 min og press til slutt ned stempelet.</li>
+			<li>Drikk med en gang!</li>
+
+		</ul>
+	</div>
 
 <h2>Espresso</h2>
-	 <p>Drikken espresso er laget ved å tvinge nesten kokende vann gjennom fint malte kaffebønner.
-	 bruker en espressomaskin for å gjøre dette, men skal du enkelt lage espresso hjemme, kan du bruke en mokkakanne. En mokkakanne koster rundt 300-400 kroner og kan kjøpes i forskjellige størrelser i de fleste butikker som selger kjøkkenutstyr.
+	<p>Drikken espresso er laget ved å tvinge nesten kokende vann gjennom fint<br>
+	  	malte kaffebønner. Kaffesjapper bruker en espressomaskin for å gjøre dette,<br>
+	  	men skal du enkelt lage espresso hjemme, kan du bruke en <strong>mokkakanne</strong>.
+	  	Når man lager espresso bruker man kaffe som er veldig finmalt.<br>
+	  	Det kan man kjøpe ferdig kvernet i butikken, eller man kan<br>
+	  	kjøpe hele bønner selv og kverne de hjemme.
+	</p>
 
-		Når man lager espresso bruker man kaffe som er veldig finmalt. Det kan man kjøpe ferdig kvernet i butikken, eller man kan kjøpe hele bønner selv og kverne de hjemme. Det finnes både elektriske kverner og håndkverner. Håndkverner er koselige og det lukter supergodt i hele rommet når man kverner, men man går veldig fort lei av å kverne for hånd hver gang. Dessuten koster gjerne en håndkvern mer enn en elektrisk.
+	 
+	
 
-	 En mokkakanne funker slik at du heller vann i den nederste delen, fyller filterdelen med kaffe, skrur på toppdelen og setter den på stekeplaten. Trykket som skapes når vannet koker i den nederste delen presser vannet opp til kaffen og videre opp i den øverste delen. Nå den øverste halvdelen er halvfull, kan du ta den av platen. 
+	<h3>Mokkakannen</h3>
+	<p>
+		En mokkakanne koster rundt 300-400 kroner og kan kjøpes i forskjellige<br>
+		størrelser i de fleste butikker som selger kjøkkenutstyr.<br>
+		En mokkakanne funker slik at du heller vann i den nederste delen,<br>
+		fyller filterdelen med kaffe, skrur på toppdelen og setter den på<br>
+		stekeplaten. Trykket som skapes når vannet koker i den nederste<br>
+		delen presser vannet opp til kaffen og videre opp i den øverste delen.<br>
+		Nå den øverste halvdelen er halvfull, kan du ta den av platen. 
+	</p>
 
 
-	Man kan også ha melk og sukker i kaffen, men jeg vet ikke hvordan det smaker.</p>
+	<p>Man kan også ha melk og sukker i kaffen, men jeg vet ikke hvordan det smaker.</p>
