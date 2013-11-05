e0b462a0f4f703f51a85d6e7fef0dca35abba480
kristofferosen <kristofferosen@gmail.com>
2013-11-05 12:06:43 +0100

    jeg prøver meg

---
+++ www/indexjegprøvermeg.php
@@ -0,0 +1,140 @@
+<?php
+	error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" title="Standard" href="css/style.css">
+		<title>Kaffe - en kjærlighetshistorie</title>
+		<script charset="utf-8" src="external_code/jquery-1.10.2.min.js"></script>
+		<script charset="utf-8" src="js/script.js"></script>
+	</head>
+	<body>
+		<header>
+			<h1>Kaffe</h1>
+			<h3> - en kjærlighetshistorie</h3>
+		</header>
+		<article id ="content">
+			<div id ="intro">
+				<p>Kaffe er det man starter dagen med.<br>
+				Det er over en kaffe man treffer venner.<br>
+				Det er kaffen som gjør dagen så awesome.</p><br>
+				<p>Men hva er det egentlig?</p>
+			</div>
+			<div id ="hva">
+				<p> Er det en bønne?<br>
+					En frukt?<br>
+					Et bær?<br>
+					Et frø?</p>
+			</div>
+			Det man starter dagen med
+			Det treffer venner over en kopp
+			Man tar en kopp med kolleger
+			Men hva er det?
+
+			Er det en bønne?
+			En frukt?
+			Et bær?
+			Et frø?
+			Kaffe er et frø i en frukt, eller “a cherry”
+			Som vokser på kaffetrærne langs fjellsiden eller i høyden
+			- Noen ord om klimaet?
+			Fruktene høstes
+			Frøene tas ut
+			Tørkes
+			Sorteres
+			Og sendes i 60kg sekker verden rundt
+			Det er den 2. mest (lovlige) omsatte varen i verden etter olje
+			Hvordan oppdaget vi kaffen da?
+			To legender
+			gjeteren som oppdaget at geitene hans ikke sov om natta etter å spist noen frukter
+			Munken som oppdaget
+			Det vi vet derimot
+			Er at fruktene er blitt brukt som oppkvikkere siden 600tallet på afrikas horn
+			og fra ca. 1400 brukt mer som vi kjenner det i dag
+			Hvor mange kopper har du drukket i dag?
+			1?
+			2?
+			3?
+			5?
+			Godt, for er du på den 5. koppen så har du senket mulighetene for diabetes type 2 med 50%
+			Du har senket sjansen for å få en masse hjerte og kar sykdommer
+			Du utvikler demens senere enn andre
+			Og mer kaffe du drikker, jo mindre sjanse for parkinson
+			Og skal du trene nå snart?
+			Da kan kaffen hjelpe deg med å øke yteevnen med så mye som 10% 
+			Og koffeinen
+			Ikke bare er den oppkvikkende
+			Den hjelper deg å stimulere galleblærefunksjonen og kan forebygge gallestein
+			Bare for å drikke kaffe
+			Og siden kaffe stammer fra frukt
+			Kan vi da si at det er en av de fem om dagen?
+			Kaffe er 60% av antioksidant inntaket på den norske befolkningen
+			Norge fikk kaffe på 1600 tallet
+			Da som en luksusvare, for rikfolket
+			I dag drikkes det av alle sosiale lag
+			Men det har vært e gradvis klassereise
+			I dag er norge ett Kaffesentrum
+			Med kaffehandlere som Wendelboe og hva nå han andre het
+			masse enkeltstående kaffebarer og brennerier
+			-Sitat fra utenlandsk journalist
+			Hvordan tar du kaffen din?
+			Svart?
+			Med melk?
+			Som latte?
+			Fra lokale kaffebaren?
+			Har du en kopp kaffe nå?
+			Ikke? Hent en nå, jeg venterher så lenge
+			Ordnet?
+			Smaker den ikke godt?
+			Det sies at lukten av kaffe i seg selv har en helsebringende effekt
+			Lukten av velvære
+			Har du prøvd en red eye - filterkaffe med en shot espresso
+			Viatnemesisk iskaffe?
+			Og hvordan lager du den?
+			En kapsel?
+			Fra Moccamaster?
+			Presskanne?
+			Tilberedt med en chemex?
+			Slow-drip - som tar 12 timer
+			Når kaffen kommer til norge er det lyse bønner
+			Så brennes den i Norge
+			Brenningen avgjør mye av smaken
+			Mye brent gir XXXX
+			Lite brent XXX
+			Og det kan legges til aroma
+			Så kvernes kaffen
+			Enten hos kaffehuset
+			Eller kverner du bønnene selv?
+			Kjenner du deg kvikk?
+			Selvfølgelig gjør du det
+			Koffeinen tas opp nesten umiddelbart
+			En kopp kaffe inneholder opptil 200mg koffein
+			Energidrikker? Ca. 80mg
+			Er det mørkbrent kaffe inneholder den mindre koffein en lysbrent
+			Så nyt kaffen din
+			Man finner forøvrig koffein i mer enn 60 planter
+			Og 90% av jordas befolkning tar til seg koffein på et vis
+			Nyter du en bit sjokolade til kaffen din?
+			Den inneholder også koffein
+			Og smertestillende kan også inneholde koffein
+			Gjør de det er de inntil 40% mer effektive
+			og kan hjelpe kroppen til å ta det opp raskere
+			Har du opplevd å få fine mønster i kaffen din?
+			Har du opplevd å få en figur i kaffen din?
+
+			Kaffe - kaffe. Smak på ordet
+			Hvor kommer det fra?
+			Første gang dokumentert er fra 1598 på italiensk, caffe
+			Som igjen er fra tyrkisk kahve
+			Som igjen er avledet fra det arabiske ordet qahwah (kaffe). 
+			Det siste skal etter sigende bety vin, 
+			Mer sannsynlig har det sin opprinnelse fra den etiopiske regionen Kaffa, et tradisjonelt område for kaffeproduksjon. 
+			Kaffe er forøvrig kalt buno i Etiopia
+
+			Kaffe har også påvirket kultur
+		</article>
+	
+	</body>
+</html>
