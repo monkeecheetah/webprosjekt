d5d029a754bce91a1f747ed5c257a803a9924a21
kristofferosen <kristofferosen@gmail.com>
2013-11-05 12:49:21 +0100

    orger

--- www/indexjegprøvermeg.php
+++ www/indexjegprøvermeg.php
@@ -9,6 +9,8 @@
 		<title>Kaffe - en kjærlighetshistorie</title>
 		<script charset="utf-8" src="external_code/jquery-1.10.2.min.js"></script>
 		<script charset="utf-8" src="js/script.js"></script>
+		<script src="js/animations.js"></script>
+    	<script src="external_code/jarallax-0.2.3b.min"></script>
 	</head>
 	<body>
 		<header>
@@ -22,74 +24,50 @@
 				Det er kaffen som gjør dagen så awesome.</p><br>
 				<p>Men hva er det egentlig?</p>
 			</div>
-			<div id ="hva">
+
+
+			<div id ="faq">
 				<p> Er det en bønne?<br>
 					En frukt?<br>
 					Et bær?<br>
 					Et frø?</p>
 			</div>
-			Det man starter dagen med
-			Det treffer venner over en kopp
-			Man tar en kopp med kolleger
-			Men hva er det?
 
-			Er det en bønne?
-			En frukt?
-			Et bær?
-			Et frø?
-			Kaffe er et frø i en frukt, eller “a cherry”
-			Som vokser på kaffetrærne langs fjellsiden eller i høyden
-			- Noen ord om klimaet?
-			Fruktene høstes
-			Frøene tas ut
-			Tørkes
-			Sorteres
-			Og sendes i 60kg sekker verden rundt
-			Det er den 2. mest (lovlige) omsatte varen i verden etter olje
-			Hvordan oppdaget vi kaffen da?
-			To legender
-			gjeteren som oppdaget at geitene hans ikke sov om natta etter å spist noen frukter
-			Munken som oppdaget
-			Det vi vet derimot
-			Er at fruktene er blitt brukt som oppkvikkere siden 600tallet på afrikas horn
-			og fra ca. 1400 brukt mer som vi kjenner det i dag
-			Hvor mange kopper har du drukket i dag?
-			1?
-			2?
-			3?
-			5?
-			Godt, for er du på den 5. koppen så har du senket mulighetene for diabetes type 2 med 50%
-			Du har senket sjansen for å få en masse hjerte og kar sykdommer
-			Du utvikler demens senere enn andre
-			Og mer kaffe du drikker, jo mindre sjanse for parkinson
-			Og skal du trene nå snart?
-			Da kan kaffen hjelpe deg med å øke yteevnen med så mye som 10% 
-			Og koffeinen
-			Ikke bare er den oppkvikkende
-			Den hjelper deg å stimulere galleblærefunksjonen og kan forebygge gallestein
-			Bare for å drikke kaffe
-			Og siden kaffe stammer fra frukt
-			Kan vi da si at det er en av de fem om dagen?
-			Kaffe er 60% av antioksidant inntaket på den norske befolkningen
-			Norge fikk kaffe på 1600 tallet
-			Da som en luksusvare, for rikfolket
-			I dag drikkes det av alle sosiale lag
-			Men det har vært e gradvis klassereise
-			I dag er norge ett Kaffesentrum
-			Med kaffehandlere som Wendelboe og hva nå han andre het
-			masse enkeltstående kaffebarer og brennerier
-			-Sitat fra utenlandsk journalist
-			Hvordan tar du kaffen din?
+			<div id="svar">
+				<p>
+					Kaffe er et frø i en frukt, eller “a cherry”<br>
+					Som vokser på kaffetrærne langs fjellsiden eller i høyden<br>
+					Arabicatrærene vokser i fuktig klima, høyt oppe mens de<br>
+					hardføre robustatrærene vokser lavere og tåler mer</p>
+					
+				<p> Fruktene høstes, frøene tas ut, tørkes og sendes i 60kg sekker verden rundt<br></p>
+
+				<p> Det er den 2. mest (lovlige) omsatte varen i verden etter olje<br></p>
+
+			</div>
+
+
+			<div id="legends">
+				<h2>Hvordan oppdaget vi kaffen da?</h2>
+					<p>To legender
+					gjeteren som oppdaget at geitene hans ikke sov om natta etter å spist noen frukter
+					Munken som oppdaget
+					Det vi vet derimot
+					Er at fruktene er blitt brukt som oppkvikkere siden 600tallet på afrikas horn
+					og fra ca. 1400 brukt mer som vi kjenner det i dag</p>
+
+			</div>
+
+			<div id="mekking">
+				Hvordan tar du kaffen din?
 			Svart?
 			Med melk?
 			Som latte?
 			Fra lokale kaffebaren?
-			Har du en kopp kaffe nå?
-			Ikke? Hent en nå, jeg venterher så lenge
-			Ordnet?
-			Smaker den ikke godt?
-			Det sies at lukten av kaffe i seg selv har en helsebringende effekt
-			Lukten av velvære
+				Insert kaffeknapp her
+
+
+
 			Har du prøvd en red eye - filterkaffe med en shot espresso
 			Viatnemesisk iskaffe?
 			Og hvordan lager du den?
@@ -98,29 +76,83 @@
 			Presskanne?
 			Tilberedt med en chemex?
 			Slow-drip - som tar 12 timer
-			Når kaffen kommer til norge er det lyse bønner
-			Så brennes den i Norge
-			Brenningen avgjør mye av smaken
-			Mye brent gir XXXX
-			Lite brent XXX
-			Og det kan legges til aroma
-			Så kvernes kaffen
-			Enten hos kaffehuset
-			Eller kverner du bønnene selv?
-			Kjenner du deg kvikk?
-			Selvfølgelig gjør du det
-			Koffeinen tas opp nesten umiddelbart
-			En kopp kaffe inneholder opptil 200mg koffein
-			Energidrikker? Ca. 80mg
-			Er det mørkbrent kaffe inneholder den mindre koffein en lysbrent
-			Så nyt kaffen din
-			Man finner forøvrig koffein i mer enn 60 planter
-			Og 90% av jordas befolkning tar til seg koffein på et vis
-			Nyter du en bit sjokolade til kaffen din?
-			Den inneholder også koffein
-			Og smertestillende kan også inneholde koffein
-			Gjør de det er de inntil 40% mer effektive
-			og kan hjelpe kroppen til å ta det opp raskere
+
+			</div>
+		
+
+
+			<div id="helse">
+					Hvor mange kopper har du drukket i dag?
+					1?
+					2?
+					3?
+					5?
+					Godt, for er du på den 5. koppen så har du senket mulighetene for diabetes type 2 med 50%
+					Du har senket sjansen for å få en masse hjerte og kar sykdommer
+					Du utvikler demens senere enn andre
+					Og mer kaffe du drikker, jo mindre sjanse for parkinson
+					Og skal du trene nå snart?
+					Da kan kaffen hjelpe deg med å øke yteevnen med så mye som 10% 
+					Og koffeinen
+					Ikke bare er den oppkvikkende
+					Den hjelper deg å stimulere galleblærefunksjonen og kan forebygge gallestein
+					Bare for å drikke kaffe
+					Og siden kaffe stammer fra frukt
+					Kan vi da si at det er en av de fem om dagen?
+
+			</div>
+
+					
+			<div id="koffein">	
+				Kjenner du deg kvikk?
+				Selvfølgelig gjør du det
+				Koffeinen tas opp nesten umiddelbart
+				En kopp kaffe inneholder opptil 200mg koffein
+				Energidrikker? Ca. 80mg
+				Er det mørkbrent kaffe inneholder den mindre koffein en lysbrent
+				Så nyt kaffen din
+				Man finner forøvrig koffein i mer enn 60 planter
+				Og 90% av jordas befolkning tar til seg koffein på et vis
+				Nyter du en bit sjokolade til kaffen din?
+				Den inneholder også koffein
+				Og smertestillende kan også inneholde koffein
+				Gjør de det er de inntil 40% mer effektive
+				og kan hjelpe kroppen til å ta det opp raskere
+			</div>
+
+
+			<div id="norge">
+				Kaffe er 60% av antioksidant inntaket på den norske befolkningen
+			Norge fikk kaffe på 1600 tallet
+			Da som en luksusvare, for rikfolket
+			I dag drikkes det av alle sosiale lag
+			Men det har vært e gradvis klassereise
+			I dag er norge ett Kaffesentrum
+			Med kaffehandlere som Wendelboe og hva nå han andre het
+			masse enkeltstående kaffebarer og brennerier
+				-Sitat fra utenlandsk journalist
+
+			</div>
+			
+			
+
+			<div id="beanyman">
+				Når kaffen kommer til norge er det lyse bønner
+				Så brennes den i Norge
+				Brenningen avgjør mye av smaken
+				Mye brent gir XXXX
+				Lite brent XXX
+				Og det kan legges til aroma
+				Så kvernes kaffen
+				Enten hos kaffehuset
+				Eller kverner du bønnene selv?
+			</div>
+
+
+	
+
+
+
 			Har du opplevd å få fine mønster i kaffen din?
 			Har du opplevd å få en figur i kaffen din?
 
