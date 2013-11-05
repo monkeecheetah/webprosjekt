0b3dbba6f76bbe291050528f6a4f3853f613459c
chrban <mac@dhcp-158-39-196-97.wlan.hioa.no>
2013-11-05 15:32:25 +0100

    teat

--- www/wwwtest/js/animations.js
+++ www/wwwtest/js/animations.js
@@ -2,8 +2,8 @@ init = function(){
       jarallax = new Jarallax();
 
 
-      jarallax.setDefault('.content div', {opacity:'0'});
-      jarallax.setDefault('.content div', {marginLeft:'-1000px'});
+      jarallax.setDefault("p", {opacity:'0'});
+      jarallax.setDefault('p', {marginLeft:'-1000px'});
 
 
       jarallax.addAnimation('.bak1',[{progress: "0%", top:"-10%"}, {progress: "100%", top: "-10%"}]);		
