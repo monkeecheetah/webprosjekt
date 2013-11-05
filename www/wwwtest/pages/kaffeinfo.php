ea84a45576fcea4f78a2c0f68e280ecbd9d4b83e
chrban <mac@dhcp-158-39-196-97.wlan.hioa.no>
2013-11-05 12:57:41 +0100

    Oprettet testfield www/wwwtest/

---
+++ www/wwwtest/pages/kaffeinfo.php
@@ -0,0 +1,135 @@
+<q>A day without coffee is like... a day without coffee</q>
+<h1>Hva er kaffe?</h1>
+<h2>Kaffehistorie</h2>
+<h3>To legender.</h3> 
+<p>Ene om gjeteren Kaldi. Etiopisk gjeter som oppdaget at geitene ikke sov etter å spist røde bær. Når det ble presentert for det lokale klosteret ble det kalt djevelens verk, kastet i peisen. Det skulle så kastes og ble brukt kaldt vann for å kjøle de ned, som så ga en såpass god lukt at de måtte prøve det. Den andre er om  Sheik Abou'l Hasan Schadheli' sin disippel Omar som siden ble helgen for innføringen av kaffe.</p>
+<p>Det er også sagt at kaffebønnene sin første bruk var at de ble spist hele og råe, eller blandet med dyrefett og spist på lange turer siden 5-600 tallet i Etiopia.</p>
+
+<p>Første dokumenterte kaffen som vi kjenner den i dag er fra Yemen på 1400tallet hvor det er brukt på mange måter som i dag. Og derfra spredde det seg både i bruk og med handel.</p>
+<p>Kilder: <a href="http://coffeetea.about.com/od/advancedcoffee/a/The-Origin-Of-Coffee.htm">http://coffeetea.about.com/od/advancedcoffee/a/The-Origin-Of-Coffee.htm</a> <a href="http://www.kaffeguiden.info/historie.html">http://www.kaffeguiden.info/historie.html</a>
+</p>
+
+<h2>Fra bønne til kopp (en reise fra bønne til kopp)</h2>
+<p><a href="http://equalexchange.coop/products/coffee/steps">http://equalexchange.coop/products/coffee/steps</a></p>
+<p><a href="http://www.friele.no/alt-om-kaffe/fra-bonne-til-kopp">http://www.friele.no/alt-om-kaffe/fra-bonne-til-kopp</a></p>
+<h3>Kaffetreet</h3>
+<p>Kaffetre som har kaffefrukter, som også kalles bær</p>
+<p>I kaffefruktene er det frø, som på folkemunne kalles bønner</p>
+<h3>Hva smaker kaffefrukten?</h3>
+<p>Noen påstår de er søte med klissete kjøtt, som smaker som vannmelon, rosenvann og hibiskus</p>
+<p><a href="http://drinks.seriouseats.com/2011/02/what-does-a-coffee-cherry-tastes-like.html">http://drinks.seriouseats.com/2011/02/what-does-a-coffee-cherry-tastes-like.html</a></p>
+<h3>Robusta vs arabica. To bønnetyper.</h3>
+<h3>Høsting</h3>
+<p>Fruktene plukkes</p>
+<h3>Fjerning av fruktkjøtt</h3>
+<h4>Den tørre metoden</h4>
+<h5>Natural og pulped natural</h5>
+<p>Natural så tørkes bærene før frukten rundt fjernes
+<p>Pulped natural - Frøene separeres fra frukten før det tørkes</p>
+
+<h4>Den våte metoden</h4>
+<h5>Fermentering</h5>
+<p>Ved å legge kaffebærene i bløt løsner det ytterste skinnlaget på fruktkjøttet. Dette gjør det enklere å fjerne selve fruktkjøttet, såkal "pulping". En pulpe-maskin har hull/rister hvor kun kaffebønnen kan presses gjennom og gå videre i systemet. Det som er igjen er bønnen med en sølvhinne og pergament fremdeles inntakt, men med rester av fruktkjøttet som er som et planteslim.</p>
+<p>Vanlige betegnelser er "Semi-washed" og "Fully washed"</p>
+<h3>Tørking</h3>
+<p>Frøene tørkes i solen eller mekanisk(?). Denne prosessen kan ta alt fra noen dager til flere uker. Avhengig av været. Når den har blitt tørket til 12% fuktighet blir frøene lagt i sekkert og klare for eksport</p>
+<h3>Sortering</h3>
+<p>Frøene blir sortert for å luke ut dårliger bønner. Dette gjøres enten med maskin, for hånd eller for hånd ved et transport bånd</p>
+<h3>Brenning/røsting</h3>
+<p>Kaffebønnene er grønne før de blir brent. Dette er en viktig del av prosessen og avgjør hvor mye og hvilken smak kaffen skal få. De brennes på henimot 200 °C til fargen blir sjokoladebrun. Ved rostingen får kaffe sin særpregede aroma. Eksempler på hvordan de skal ristes er "medium roast", "full city roast", "vienna roast", "French roast" og "blending" (skrive mer om disse typene brenning?</p>
+<h3>Kvalitetstesting</h3>
+<p>Det blir tatt smaksprøver på kaffen om de er holder mål og det blir gitt tilbakemelding til kaffebøndene om hvordan kaffen er. Hvordan dette gjøres varierer fra kaffehus til kaffehus</p>
+<h3>Kverning</h3>
+<p>Her regner jeg med at Osen har skrevet ett og annet</p>
+<p>Veldig bredt og generelt om kaffe <a href="http://snl.no/kaffe">http://snl.no/kaffe</a></p>
+
+<h2>Kaffe og koffein</h2>
+<p>Kaffe inneholder ca. 110mg koffein i en kopp (ca. 240ml) Avhenger av type, størrelse på kopp osv. Men mer enn i en boks red bull som bare inneholder 80mg i en 250ml boks.
+<p><a href="http://www.energyfiend.com/caffeine-content/coffee-brewed">http://www.energyfiend.com/caffeine-content/coffee-brewed</a></p>
+<h2>Kaffekunst</h2>
+<p><a href="http://www.amusingplanet.com/2013/05/kazuki-yamamotos-incredible-3d-coffee.html">http://www.amusingplanet.com/2013/05/kazuki-yamamotos-incredible-3d-coffee.html</a></p>
+<h2>Opphav på navnet</h2>
+<p>“Selv ordet kaffe er dokumentert fra 1598 på italiensk, caffe, igjen fra tyrkisk kahve, som igjen er avledet fra det arabiske ordet qahwah (kaffe). Det siste skal etter sigende bety vin, men mer sannsynlig er nok forklaringen at ordet har sin opprinnelse i den etiopiske regionen Kaffa, et tradisjonelt område for kaffeproduksjon. Kaffe er forøvrig kalt buno i Etiopia”</p>
+<p><a href="Fra wikipedia artikkelen http://no.wikipedia.org/wiki/Kaffe">Fra wikipedia artikkelen http://no.wikipedia.org/wiki/Kaffe</a> <a href="http://www.etymonline.com/index.php?search=coffee&searchmode=none">http://www.etymonline.com/index.php?search=coffee&searchmode=none</a></p>
+
+<h2>Mokka - både bønnetype, tilberedningsteknikk og sjokoladelatte.</h2>
+
+<h2>Produksjonsland</h2>
+<p>I 2009 var Brasil verdensledende innen produksjon av grønn kaffe, etterfulgt av Vietnam, Indonesia og Colombia
+Basert på denne, tatt fra Wikipedia.</p>
+<p><a href="http://www.fas.usda.gov/htp/coffee/2009/December_2009/2009_coffee_december.pdf">http://www.fas.usda.gov/htp/coffee/2009/December_2009/2009_coffee_december.pdf</a></p>
+<h3>Topp 10 kaffe produserende land</h3>
+<ol>
+	<li>Brazil</li>
+	<li>Vietnam</li>
+	<li>Elfenbenskysten</li>
+	<li>Indonesia</li>
+	<li>Etiopia</li>
+	<li>India</li>
+	<li>Mexico</li>
+	<li>Guatemala</li>
+	<li>Peru</li>
+	<li>Honduras</li>
+</ol>
+<h4>Særpreg fra forskjellige regioner</h4>
+<h3>Kaffe og handel</h3>
+<p>Kaffe på det internasjonale markedet måles i sekker og hver sekk veier 60kg</p>
+<h2>Kaffe og kultur</h2>
+<ul>
+	<li>Coffee and cigarettes</li>
+	<li><a href="http://comediansincarsgettingcoffee.com/">Comedians in car getting coffee</a></li>
+	<li>GTA's Hot coffee</li>
+	<li>Blurs Coffee&TV</li>
+</ul>
+<h2>Helseeffekter</h2>
+<h3>Kan kaffe med andre ord erstatte en av de 5 om dagen?</h3>
+<p>Det er de samme stoffene som finnes i frukt og grønnsaker, og som man tror kan forklare den positive effekten av å spise frukt og grønt</p>
+<p>Mange bl.a. hjerte-og karsykdommer. kaffe reduserer risikoen for diabetes 2, bra for korttidsminnet og beskytte mot skrumplever.  “- Kaffe inneholder mange polyfenoler, hvorav de aller fleste er gode antioksidanter. Det er de samme stoffene som finnes i frukt og grønnsaker, og som man tror kan forklare den positive effekten av å spise frukt og grønt, sier Blomhoff.”</p>
+
+<p><a href="http://www.forskning.no/artikler/2006/mai/1148455804.23">http://www.forskning.no/artikler/2006/mai/1148455804.23</a></p>
+<h2>Rare kaffedrikker</h2>
+<h3>Svart kaffe</h3>
+<h4>Kaffe Mike Tyson - Svart og uten kjærlighet (sukker)</h4>
+<h3>Melkebaserte kaffedrikker</h3>
+<h3>Iskaffe</h3>
+<h4>Vietnamesisk iskaffe</h4>
+<h2>Funfacts</h2>
+<h3>Kafé og andre opphavsord</h3>
+<ul>
+	<li>Ordet kafé stammer fra ordet kaffe, og skulle være serveringssteder uten alkohol. Bl.a. i muslimske land. Enda myndighetene forsøkte å hindre det, ble bruken av kaffe i det følgende århundre stadig mer vanlig i hele Europa. Det ble den gang antatt at det nye nytelsesmiddel ville redusere alkoholforbruket.<a href="http://snl.no/kaf%C3%A9">http://snl.no/kaf%C3%A9</a></li>
+	<li>Coffetable</li>
+	<li>Coffetablebook (jfr Seinfeld)</li>
+	<li>Kaffebrød</li>
+	<li>Kaffe/mokka-farget</li>
+</ul>
+<h3>Kaffekonsum verden over</h3>
+<ol>
+	<li>Finland</li>
+	<li>Norge</li>
+	<li>Danmark</li>
+	<li>Nederland</li>
+	<li>Sverige</li>
+	<li>Sveits</li>
+	<li>Beliga</li>
+	<li>Tyskland</li>
+	<li>Canda</li>
+	<li>Estland</li>
+</ol>
+<p><a href="http://www.worldmapper.org/posters/worldmapper_1038_coffee_consumption_ver2.pdf">http://www.worldmapper.org/posters/worldmapper_1038_coffee_consumption_ver2.pdf</a></p>
+<h3>Lang liste med facts. Kilder i bunn om vi velger å gå for noen av disse</h3>
+<p><a href="http://en.ilovecoffee.jp/posts/view/92">http://en.ilovecoffee.jp/posts/view/92</a></p>
+
+<h3>Søt historie om hvorfor kaffe serveres på kaffeservise</h3>
+<p><a href="http://en.ilovecoffee.jp/posts/view/83">http://en.ilovecoffee.jp/posts/view/83</a></p>
+
+<h2>Kaffe og ansvar, noe vi skal se nærmere på?</h2>
+<p>En kopp kaffe trenger 140 liter vann</p>
+<p>Kaffe dyrkes i land med allerede dårlig tilgang på ferskvann som Etiopia og Kenya</p>
+<p>Det sies at verden trenger 25-30millioner flere kaffesekker i 2020. Dette fordi yngre generasjoner i land som Kina og India har begynt å drikke kaffe</p>
+<p>For å dekke verdens behov må kaffen bli dyrere - så bønder kan investere i det utstyret som trengs for å effektivisere produksjonen sin</p>
+<p>Ved hjelp av kunnskapsutveksling og investeringer i moderne produksjonsmetoder og infrastruktur kan en kaffebonde så mye som tidoble produksjonen sin</p>
+<p>Noen vil påstå Starbucks har vært en pådriver for mer bærekraftig kaffehandel og høyere kvalitet på kaffen som drikkes (særlig i statene)</p>
+<p><a href="http://www.nationen.no/2013/09/16/naring/dagens_kommentar/kommentar/kaffe/fairtrade/8274215/">http://www.nationen.no/2013/09/16/naring/dagens_kommentar/kommentar/kaffe/fairtrade/8274215/</a></p>
+<p><a href="http://www.kaffe.no/verden-trenger-mer-kaffe-til-l%C3%B8nnsomme-priser/">http://www.kaffe.no/verden-trenger-mer-kaffe-til-l%C3%B8nnsomme-priser/</a></p>
+<p><a href="http://www.waterfootprint.org/Reports/Report14.pdf">http://www.waterfootprint.org/Reports/Report14.pdf</a></p>
+<p><a href="http://coffeelands.crs.org/2012/09/302-the-water-footprint-of-your-coffee/">http://coffeelands.crs.org/2012/09/302-the-water-footprint-of-your-coffee/</a></p>
---
+++ www/wwwtest/pages/tilberedning.php
@@ -0,0 +1,99 @@
+<h1>Tilbredning</h1>
+
+ <h2>Råvarer</h2>
+ 	<p>
+ 		Råvarene er viktigst! Kjøp hele bønner.<br>
+ 		Kvern dem selv! Bruk eletrisk kvern eller<br> 
+ 		håndkvern hvis du gidder.
+ 	</p>
+
+
+ <h2>Brygg manuelt med med papirfilter</h2>
+ 	<p>Hvis du brygger kaffen for hånd, ikke med kaffetrakter, <br>
+ 	   får du mer kontroll på hva den skal smake!
+ 	</p>
+
+ 	<h2>Teknikker</h2>
+ 		<h3>Chemex</h3>
+
+ 		<h3>Vakumkolbe</h3>
+ 			<p>Var poppis i Norge på 60tallet og ser superkul ut<br>
+
+ 			</p>
+
+
+	 	<p>Hvis man vil ha litt mer kontroll over filterbrygging kan man<br>
+	 	   bruke de mer manuelle teknikkersom inngår i <strong>håndbrygging</strong>.<br>
+	 	   Det som er mest i vinden for tiden er å bruke en<br>
+	 	   filterholder,(lignende de som brukes på en vanlig kaffetrakter)<br>
+	 	   med spesielt tilpassede filtre, som man plasserer på kaffekoppen<br>
+	 	   og dermed brygger rett i koppen.<br><br>
+
+	 	   Dette er som sagt en metode som er veldig i vinden for tiden, og<br>
+	 	   på internett finnes det utallige kaffenerder som har sin egen måte<br>
+	 	   å håndbrygge på, noe som gjør håndbrygging vanskelig å forklare. 
+
+	 	   Hovedpoenget er uansett at du som brygger har større <Strong>kontroll</strong><br>
+	 	   over prosessen og kan lettere kontrollere hvilke kvaliteter kaffen skal ha.
+	 	</p>
+
+
+	<h3>Aero-press</h3>
+		<p>jeg blir lei av ordspill som “den heteste kaffetrenden” og “hotte kaffetrender” og skriver om dette når jeg har roet meg, hehe</p>
+
+
+<h2>Prezzkanne</h2>
+	<div id="pressinfo">
+
+	 	<p>Presskanne er en rask og enkel måte å lage mye og fyldig kaffe på.<br>
+	 	   Du kan bruke mange forskjellige typer kaffe i en presskanne.<br>
+	 	   Både <em>filter</em>- og <em>kokmalt</em> funker. Du kan også kjøpe<br>
+	 	   kaffe som er kvernet spesielt for presskanne. Den beste veien til god<br>
+	 	   presskaffe er likevel å kjøpe hele bønner og kverne dem selv. <br>
+	 	   Kvern bønnene grovt, så de ikke slipper igjennom metallfilteret<br>
+	 	</p>
+ 	</div>
+
+ 	<div id="presstips">
+		<p>En kanne kan du lage lage enkelt, ved å hive oppi en "fornuftig" mengde kaffe,<br>
+		   kokende vann og presse ned etter et par minutter. Hvis du vi eksprimentere<br>
+		   litt mer med smaken kan du være litt mer nøye. Kaffenerder og barristaer har<br>
+		   laget kaffe i presskanne i mangemange år og har funnet ut hva som funker best.<br>
+		   Her er noen tips:
+		</p>
+		<ul>
+			<li>Skal du lage en hel presskanne, som vanligvis er en liter, bør du bruke rundt 60 gram bønner. </li>
+			<li>Kannen burde du forvarme med varmt vann før du har kaffen i.</li>
+			<li>Vannet du bruker skal ikke koke, men være rundt 90-96 grader</li>
+			<li>Hell i vannet i to runder. Fyll kannen halveis opp først og rør rundt, før du heller i resten av vannet.</li>
+			<li>La kaffe kose seg i ganske nøyaktig 4 min og press til slutt ned stempelet.</li>
+			<li>Drikk med en gang!</li>
+
+		</ul>
+	</div>
+
+<h2>Espresso</h2>
+	<p>Drikken espresso er laget ved å tvinge nesten kokende vann gjennom fint<br>
+	  	malte kaffebønner. Kaffesjapper bruker en espressomaskin for å gjøre dette,<br>
+	  	men skal du enkelt lage espresso hjemme, kan du bruke en <strong>mokkakanne</strong>.
+	  	Når man lager espresso bruker man kaffe som er veldig finmalt.<br>
+	  	Det kan man kjøpe ferdig kvernet i butikken, eller man kan<br>
+	  	kjøpe hele bønner selv og kverne de hjemme.
+	</p>
+
+	 
+	
+
+	<h3>Mokkakannen</h3>
+	<p>
+		En mokkakanne koster rundt 300-400 kroner og kan kjøpes i forskjellige<br>
+		størrelser i de fleste butikker som selger kjøkkenutstyr.<br>
+		En mokkakanne funker slik at du heller vann i den nederste delen,<br>
+		fyller filterdelen med kaffe, skrur på toppdelen og setter den på<br>
+		stekeplaten. Trykket som skapes når vannet koker i den nederste<br>
+		delen presser vannet opp til kaffen og videre opp i den øverste delen.<br>
+		Nå den øverste halvdelen er halvfull, kan du ta den av platen. 
+	</p>
+
+
+	<p>Man kan også ha melk og sukker i kaffen, men jeg vet ikke hvordan det smaker.</p>
