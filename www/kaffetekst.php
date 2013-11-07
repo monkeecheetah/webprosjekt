8e15d2374b4842beb439f858440c0d789113160f
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-07 14:46:32 +0100

    Da er teksten jeg hadde overført til html tagger.

---
+++ www/kaffetekst.php
@@ -0,0 +1,180 @@
+<div id="intro">
+	<h1>Kaffe - en kjærlighetshistorie</h1>
+	<p>Det man starter dagen med</p>
+	<p>Det treffer venner over en kopp</p>
+	<p>Man tar en kopp med kolleger</p>
+</div>
+<div id="hvaerkaffe">
+	<h3>Men hva er det?</h3>
+	<p>Er det en bønne?
+	<p>En frukt?</p>
+	<p>Et bær?</p>
+	<p>Et frø?</p>
+	<p>Kaffe er et frø i en frukt, eller “a cherry”</p>
+	<p>Som vokser på kaffetrærne langs fjellsiden eller i høyden</p>
+	<p>Noen ord om klimaet?</p>
+	<h3>Fra bønne til kopp</h3>
+	<p>Fruktene høstes</p>
+	<p>Frøene tas ut</p>
+	<p>Tørkes</p>
+	<p>Sorteres</p>
+	<p>Og sendes i 60kg sekker verden rundt</p>
+	<p>Det er den 2. mest (lovlige) omsatte varen i verden etter olje</p>
+</div>
+<div id="oppdagelsen">
+	<h3>Hvordan oppdaget vi kaffen da?</h3>
+	<p>Det finnes to veldig kjente anekdoter</p>
+	<p>gjeteren som oppdaget at geitene hans ikke sov om natta etter å spist noen frukter</p>
+	<p>Munken som oppdaget</p>
+	<p>Det vi vet derimot</p>
+	<p>Er at fruktene er blitt brukt som oppkvikkere siden 600tallet på afrikas horn</p>
+	<p>og fra ca. 1400 brukt mer som vi kjenner det i dag</p>
+</div>
+<div id="kaffetilnorge">
+	<h3>Enn veien til Norge da</h3>
+	<p>Norge fikk kaffe på 1600 tallet</p>
+	<p>Da som en luksusvare, for rikfolket</p>
+	<p>I dag drikkes det av alle sosiale lag</p>
+	<p>Men det har vært e gradvis klassereise</p>
+	<p>I dag er norge ett Kaffesentrum</p>
+	<p>Med kaffehandlere som Wendelboe og hva nå han andre het</p>
+	<p>masse enkeltstående kaffebarer og brennerier</p>
+	<p>-Sitat fra utenlandsk journalist</p>
+</div>
+<div id="helse">
+	<h3>Forresten;</h3>
+	<p>Hvor mange kopper har du drukket i dag?</p>
+	<p>1?</p>
+	<p>2?</p>
+	<p>3?</p>
+	<p>5?</p>
+	<p>Godt, for er du på den 5. koppen så har du senket mulighetene for diabetes type 2 med 50%</p>
+	<p>Du har senket sjansen for å få en masse hjerte og kar sykdommer</p>
+	<p>Du utvikler demens senere enn andre</p>
+	<p>Og mer kaffe du drikker, jo mindre sjanse for parkinson</p>
+	<p>Og skal du trene nå snart?</p>
+	<p>Da kan kaffen hjelpe deg med å øke yteevnen med så mye som 10% </p>
+</div>
+<div id="koffein">
+<h3>Koffeinen</h3>
+<p>Ikke bare er den oppkvikkende</p>
+<p>Den hjelper deg å stimulere galleblærefunksjonen og kan forebygge gallestein</p>
+<p>Bare ved å drikke kaffe</p>
+<p>Rekorden på mest på kortest tid</p>
+<p>- er 82 kopper på 100 timer</p>
+<p>Og om du er du redd for å drikke for mye kaffe?</p>
+<p>Vit at det skal 100 kopper til for å drepe deg</p>
+<p>Og siden kaffe stammer fra frukt</p>
+<p>Kan vi da si at det er en av de fem om dagen?</p>
+<p>Kaffe er 60% av antioksidant inntaket på den norske befolkningen</p>
+</div>
+
+<div id="hvordankaffen">
+	<h3>Hvordan tar du kaffen din?</h3>
+	<p>Svart?</p>
+	<p>Med melk?</p>
+	<p>Som latte?</p>
+	<p>Fra lokale kaffebaren?</p>
+	<p>Har du en kopp kaffe nå?</p>
+	<p>Ikke? Hent en nå, jeg venterher så lenge</p>
+	<p>Ordnet?</p>
+	<p>Smaker den ikke godt?</p>
+	<p>Det sies at lukten av kaffe i seg selv har en helsebringende effekt</p>
+	<p>Lukten av velvære</p>
+	<p>Har du prøvd en red eye - filterkaffe med en shot espresso</p>
+	<p>Viatnemesisk iskaffe?</p>
+	<p>Og hvordan lager du den?</p>
+	<p>En kapsel?</p>
+	<p>Fra Moccamaster?</p>
+	<p>Presskanne?</p>
+	<p>Tilberedt med en chemex?</p>
+	<p>Slow-drip - som tar 12 timer</p>
+	<p>Når kaffe eksporteres er den lysegrønn nesten hvit</p>
+	<p>Så brennes den i Norge</p>
+	<p>Brenningen avgjør mye av smaken</p>
+	<p>Mye brent gir XXXX</p>
+	<p>Lite brent XXX</p>
+	<p>Og det kan legges til aroma</p>
+	<p>Så kvernes kaffen</p>
+	<p>Enten hos kaffehuset</p>
+	<p>Eller kverner du bønnene selv?</p>
+</div>
+<div id="merkoffein">
+<h3>Kjenner du deg kvikk?</h3>
+<p>Selvfølgelig gjør du det</p>
+<p>Koffeinen tas opp nesten umiddelbart</p>
+<p>En kopp kaffe inneholder opptil 200mg koffein</p>
+<p>Energidrikker? Ca. 80mg. </p>
+<p>Og en stor kaffe fra Starbucks har like mye koffein som 9.5 bokser cola</p>
+<p>Og er kaffe din lysbrent inneholder den mer koffein enn om den er  mørkbrent </p>
+<p>Nyt kaffen din</p>
+<p>Du har glede av den</p>
+<p>Mentalt</p>
+<p>Og fysisk</p>
+<p>Man finner forøvrig koffein i mer enn 60 planter</p>
+<p>Og 90% av jordas befolkning tar til seg koffein på et vis</p>
+<p>Nyter du en bit sjokolade til kaffen din?</p>
+<p>Den inneholder også koffein</p>
+<p>Og smertestillende kan også inneholde koffein</p>
+<p>Gjør de det er de inntil 40% mer effektive</p>
+<p>og kan hjelpe kroppen til å ta det opp raskere</p>
+<p>Har du opplevd å få fine mønster i kaffen din?</p>
+<p>Har du opplevd å få en figur i kaffen din?</p>
+<p>Kaffebarer har lenge servert cappuccino med et hjerte i toppen</p>
+<p>Nå skal det være 3d-figurer i skummet</p>
+<p>Hva blir det neste?</p>
+</div>
+<div id="ordetkaffe">
+	<h3>Kaffe - kaffe. Smak på ordet</h3>
+	<p>Hvor kommer det fra?</p>
+	<p>Første gang dokumentert er fra 1598 på italiensk, caffe</p>
+	<p>Som igjen er fra tyrkisk kahve</p>
+	<p>Som igjen er avledet fra det arabiske ordet qahwah (kaffe). </p>
+	<p>Det siste skal etter sigende bety vin, </p>
+	<p>Mer sannsynlig har det sin opprinnelse fra den etiopiske regionen Kaffa, et tradisjonelt område for kaffeproduksjon.</p>
+	<p>Og ordet café - ja du har alltid antatt det riktige. Det sxtammer fra ordet kaffe </p>
+	<p>Kaffe er forøvrig kalt buno i Etiopia</p>
+</div>
+<div id="prodland">
+<h3>Etiopia</h3>
+	<p>Etiopa. Hvor kaffe kom fra, og fortsatt kommer fra</p>
+	<p>Er i dag det 5. mest produserende landet</p>
+	<p>Topp 10 er forøvrig</p>
+	<p>1/3 av kaffen kommer faktisk fra Brasil.</p>
+	<ol>
+		<li>Brazil</li>
+		<li>Vietnam</li>
+		<li>Elfenbenskys</ten</li>
+		<li>Indonesia</li>
+		<li>Etiopia</li>
+		<li>India</li>
+		<li>Mexico</li>
+		<li>Guatemala</li>
+		<li>Peru</li>
+		<li>Honduras</li>
+	</ol>
+	<p>Har du forresten prøvd cà phê đá?</p>
+	<p>Eller vietnamesisk iskaffe som vi kaller, det kan gjøres med å lage vietnamesisk kaffe rett i et glass med isbiter, og så tilsetter man kondensert melk</p>
+	<p>(og man kan tilsette alkohol, f.eks. whisky)</p>
+	<p>Apropos, whisky. Hadde vært gøy å koblet whisky og kaffe. Det må vi få til.</p>
+</div>
+<div id="kaffeogsamfunn">
+	<h3>Kaffe og samfunn</h3>
+	<p>Kaffe er i dag en del av samfunnet</p>
+	<p>Kaffe er overalt der vi ferdes</p>
+	<p>Man møter venner over en kopp kaffe</p>
+	<p>Det er aldri et møte uten kaffe</p>
+	<p>Man har kirkekaffe</p>
+</div>
+
+<div id="kaffeansvar">
+	<h3>Fra kaffen såes i jorda</h3>	
+	<p>- til den er i din kopp</p>
+	<p>Trengs det 140 liter vann</p>
+	<p>Kaffe dyrkes i land med allerede dårlig tilgang på ferskvann som Etiopia og Kenya</p>
+	<p>Tettbefolkede land som India og Kina har en oppvoksende generasjon som har begynt å få smaken for kaffe</p>
+	<p>Det sies at verden trenger 25-30millioner flere kaffesekker i 2020. </p>
+	<p>Ved hjelp av kunnskapsutveksling og investeringer i moderne produksjonsmetoder og infrastruktur kan en kaffebonde så mye som tidoble produksjonen sin</p>
+	<p>For å dekke verdens behov må kaffen bli dyrere</p>
+	<p>Så kaffebønder kan investere i det utstyret og kunnskapen som trengs for å effektivisere produksjonen sin</p>
+</div>
