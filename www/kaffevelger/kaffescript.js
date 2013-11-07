3cab604ae1abff2fd104ab5b0256015c72fd07c1
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-07 13:55:36 +0100

    Kaffevelger ver 2.0

--- www/kaffevelger/kaffescript.js
+++ www/kaffevelger/kaffescript.js
@@ -1,19 +1,38 @@
 function init() {
 	displayCoffee();
 	$('#kaffevelger input[name=tid]:radio').change(function(){
+		$('#showAll').prop('checked', false); 
 		displayCoffee();
 	});
 	$('#kaffevelger input[name=okonomi]:radio').change(function(){
+		$('#showAll').prop('checked', false); 
+		displayCoffee();
+	});
+	$('#kaffevelger input[name=pavirke]:radio').change(function(){
+		$('#showAll').prop('checked', false); 
+		displayCoffee();
+	});
+	$('#kaffevelger input[name=bryr]:radio').change(function(){
+		$('#showAll').prop('checked', false); 
+		displayCoffee();
+	});		
+	$('#showAll').change(function(){
+		displayCoffee();
 	});
 }
 
 function displayCoffee() {
 	$('#maskinliste').html("");
 	var tid = $('#kaffevelger input[name=tid]:checked').attr('value');
+	var penger = $('#kaffevelger input[name=okonomi]:checked').attr('value');
+	var bryr = $('#kaffevelger input[name=bryr]:checked').attr('value');
+	var pavirke = $('#kaffevelger input[name=pavirke]:checked').attr('value');		
 	$.getJSON('kaffe.json', function(data){
 		$.each(data.coffee.methods, function (index, value) {
 			console.log(tid);
-			if(this.t == tid){
+			if($('#showAll').is(':checked')) {
+				$('#maskinliste').append(this.name+"<br/>");
+			} else if(this.t == tid && this.o == penger && this.b == bryr && this.p == pavirke){
 				$('#maskinliste').append(this.name+"<br/>");
 
 				console.log(this.name);
--- www/kaffevelger/kaffevelger.php
+++ www/kaffevelger/kaffevelger.php
@@ -10,22 +10,38 @@
 	</head>
 	<body onload="init()">
 
-		<div id="kaffevelger">
-			
-			<input type="radio" name="tid" value="t1" checked>
-			<label for="t1">Dårlig tid</label>
-			<input type="radio" name="tid" value="t2">
-			<label for="t2">Passe</label>
-			<input type="radio" name="tid" value="t3"><label for="t3">God tid</label>
-			<br/>
-			<input type="radio" name="okonomi" value="o1" checked>			
-			<label for="o1">Dårlig råd</label>
-			<input type="radio" name="okonomi" value="o2">
-			<label for="o2">Passe</label>
-			<input type="radio" name="okonomi" value="o3">
-			<label for="o3">God råd</label>
-		</div>
-<div id="maskinliste"></div>
+		<form id="kaffevelger">
+			<h3>Hvor god tid har du?</h3>			
+			<input type="radio" name="tid" id="tid1" value="t1" checked>
+			<label for="tid1">Dårlig tid</label>
+			<input type="radio" name="tid" id="tid2" value="t2">
+			<label for="tid2">Passe</label>
+			<input type="radio" name="tid" id="tid3" value="t3">
+			<label for="tid3">God tid</label>
+			<h3>Hvordan er pengboka?</h3>
+			<input type="radio" name="okonomi" id="oko1" value="o1" checked>			
+			<label for="oko1">Tynn</label>
+			<input type="radio" name="okonomi" id="oko2" value="o2">
+			<label for="oko2">Passe</label>
+			<input type="radio" name="okonomi" id="oko3" value="o3">
+			<label for="oko3">Feit og god</label>
+			<h3>Jeg vil påvirke prosessen</h3>
+			<input type="radio" name="pavirke" id="pros1" value="p1" checked>			
+			<label for="pros1">Ja</label>
+			<input type="radio" name="pavirke" id="pros2" value="p2">
+			<label for="pros2">Nei</label>
+			<h3>Jeg bryr meg om kaffe?</h3>
+			<input type="radio" name="bryr" id="bryr1" value="b1" checked>			
+			<label for="bryr1">Ja</label>
+			<input type="radio" name="bryr" id="bryr2" value="b2">
+			<label for="bryr2">Nei</label>
+			<br>
+			<input id="showAll" checked type="checkbox" name="showAll" value="all">
+			<label for="showAll">Vis alle muligheter</label>
+		</form>
+
+		<div id="maskinliste"></div>
+
 
 	</body>	
 </html>
