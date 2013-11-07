c91a79bda2532f6264cef12010c6504d8caf2d23
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-07 16:30:22 +0100

    Oppdatert kaffevelger

--- www/kaffevelger/kaffescript.js
+++ www/kaffevelger/kaffescript.js
@@ -8,7 +8,7 @@ function init() {
 		$('#showAll').prop('checked', false); 
 		displayCoffee();
 	});
-	$('#kaffevelger input[name=pavirke]:radio').change(function(){
+	$('#kaffevelger input[name=egeninnsats]:radio').change(function(){
 		$('#showAll').prop('checked', false); 
 		displayCoffee();
 	});
@@ -26,13 +26,13 @@ function displayCoffee() {
 	var tid = $('#kaffevelger input[name=tid]:checked').attr('value');
 	var penger = $('#kaffevelger input[name=okonomi]:checked').attr('value');
 	var bryr = $('#kaffevelger input[name=bryr]:checked').attr('value');
-	var pavirke = $('#kaffevelger input[name=pavirke]:checked').attr('value');		
+	var egeninnsats = $('#kaffevelger input[name=egeninnsats]:checked').attr('value');		
 	$.getJSON('kaffe.json', function(data){
 		$.each(data.coffee.methods, function (index, value) {
 			console.log(tid);
 			if($('#showAll').is(':checked')) {
 				$('#maskinliste').append(this.name+"<br/>");
-			} else if(this.t == tid && this.o == penger && this.b == bryr && this.p == pavirke){
+			} else if(this.t == tid && this.o == penger && this.b == bryr && this.e == egeninnsats){
 				$('#maskinliste').append(this.name+"<br/>");
 				console.log(this.name);
 			}
