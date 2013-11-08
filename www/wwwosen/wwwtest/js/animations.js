e279305c4007654fc9d6857d6a59f55e07b23d77
kristofferosen <kristofferosen@gmail.com>
2013-11-08 21:25:03 +0100

    heppdiduu

--- www/wwwosen/wwwtest/js/animations.js
+++ www/wwwosen/wwwtest/js/animations.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
       console.log("Jeg spiser havregrøt til min kaffe");
 
 
-      jarallax.addAnimation('.bak1',[{progress: "0%"}, {progress: "100%", top: "50%"}]); 
-      jarallax.addAnimation('.bak1',[{progress: "0%"}, {progress: "100%", top: "250%"}]); 
-      jarallax.addAnimation('.bak1',[{progress: "0%"}, {progress: "100%", top: "450%"}]); 
-      jarallax.addAnimation('.bak1',[{progress: "0%"}, {progress: "100%", top: "650%"}]); 
-      jarallax.addAnimation('.bak1',[{progress: "0%"}, {progress: "100%", top: "850%"}]);
-      jarallax.addAnimation('.bak1',[{progress: "0%"}, {progress: "100%", top: "1050%"}]);  
+      jarallax.addAnimation('.bak1',[{progress: "0%", top:"0"}, {progress: "100%", top: "50%"}]); 
+      jarallax.addAnimation('.bak2',[{progress: "0%", top:"200"}, {progress: "100%", top: "250%"}]); 
+      jarallax.addAnimation('.bak3',[{progress: "0%", top:"400"}, {progress: "100%", top: "450%"}]); 
+      jarallax.addAnimation('.bak4',[{progress: "0%", top:"600"}, {progress: "100%", top: "650%"}]); 
+      jarallax.addAnimation('.bak5',[{progress: "0%", top:"800"}, {progress: "100%", top: "850%"}]);
+      jarallax.addAnimation('.bak6',[{progress: "0%", top:"1000"}, {progress: "100%", top: "1050%"}]);  
 
      
    });
