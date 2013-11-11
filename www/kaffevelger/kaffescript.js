25d4449595c443604c7a15e1c8a28bc466ed1ec2
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-11 16:22:27 +0100

    changes på kaffevelger

--- www/kaffevelger/kaffescript.js
+++ www/kaffevelger/kaffescript.js
@@ -22,19 +22,25 @@ function init() {
 }
 
 function displayCoffee() {
-	$('#maskinliste').html("");
 	var tid = $('#kaffevelger input[name=tid]:checked').attr('value');
 	var penger = $('#kaffevelger input[name=okonomi]:checked').attr('value');
 	var bryr = $('#kaffevelger input[name=bryr]:checked').attr('value');
 	var egeninnsats = $('#kaffevelger input[name=egeninnsats]:checked').attr('value');		
+	console.log("tid"+tid);
+	console.log("penger"+penger);
+	console.log("bryr"+bryr);
+	console.log("egeninnsats"+egeninnsats);
+	$('#maskinliste').html("");	
 	$.getJSON('kaffe.json', function(data){
 		$.each(data.coffee.methods, function (index, value) {
-			console.log(tid);
+			console.log(this.t);
+			console.log(this.o);
+			console.log(this.e);			
 			if($('#showAll').is(':checked')) {
 				$('#maskinliste').append(this.name+"<br/>");
-			} else if(this.t == tid && this.o == penger && this.b == bryr && this.e == egeninnsats){
+//			} else if(this.t == tid  && this.o == penger && this.b == bryr && this.e == egeninnsats){
+			} else if((this.t >= tid-0.5 || this.t <= tid+0.5)  && (this.o >= penger-0.5 || this.o <= penger+0.5 ) && this.b == bryr && (this.e >= egeninnsats-0.5 || this.e <= egeninnsats+0.5)){
 				$('#maskinliste').append(this.name+"<br/>");
-				console.log(this.name);
 			}
 		});
 	});
