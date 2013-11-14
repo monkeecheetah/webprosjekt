1c9f3348196d61b951e0b59016ff90f5b1fe04cc
kristofferosen <kristofferosen@gmail.com>
2013-11-14 18:59:06 +0100

    Revurderer tekst helt og lager ny indexfil

---
+++ www/wwwMASTER/indexrevurderingavtekst.php
@@ -0,0 +1,308 @@
+<?php
+	error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Kaffe - en kjærlighetshistorie</title>
+		
+		<link rel="stylesheet" type="text/css" title="Standard" href="css/style.css">
+		<link rel="stylesheet" type="text/css" title="Standard" href="css/mediaqueries.css">
+		<link rel="stylesheet" type="text/css" title="Standard" href="css/kaffevelgerstyle.css">
+		<script src="external_code/jquery.js"></script>
+		<script src="js/script.js"></script>
+		<script src="js/kaffescript.js"></script>
+    	<meta charset="utf-8">
+	</head>
+	<body>
+			<nav>
+				<ul>
+					<li><a href="">Intro</a></li>
+					<li><a href="">Om</a></li>
+					<li><a href="#kaffevelger">Kaffevelger</a></li>	
+					<li><a href="">Velvære</a></li>														
+				</ul>
+			</nav>
+			<section class="blockWrap" id="bak0" data-speed="100" data-type="background">
+				<div class="block0 block">
+					<h1>KAFFE</h1> 
+					<h2>- en kjærlighetshistorie</h2>
+				</div>
+			</section>
+
+
+			<section class="blockWrap" id="bak1" data-speed="100" data-type="background">
+				<span id="over1" data-speed="2" data-type="over"></span>
+				<div class="block1 block">
+					<div id="intro" class="content">
+						<h3>Hva er kaffe?</h3>
+						<p>Er det en bønne?
+						<p>En frukt?</p>
+						<p>Et bær?</p>
+						<p>Et frø?</p>
+						<p>FRØ er riktig, kaffebønner er egentlig frø!</p>
+						<p>Den høstles fra de to vanligste kaffetrærene</p>
+						<p>Arabicatrærene og robustatrærene</p>
+						<p>Arabicatrærene kan vokse rundt om kring i hele verden, ikke bare i arabien</p>
+						<p>Så sendes de til Norge!<p>
+					</div>
+				</div>
+			</section>
+
+
+
+			<section class="blockWrap" id="bak2" data-speed="100" data-type="background">
+				<div class="block2 block">
+					<div id="kaffetilnorge" class="content">
+						<h3>Enn veien til Norge da</h3>
+						<p>Norge fikk kaffe på 1600 tallet</p>
+						<p>OG da var det en luksusvare, for rikfolket</p>
+						<p>I dag drikkes det heldigvis av alle sosiale lag</p>
+						<p>I dag drikker vi 3-4 kopper om dagen hver</p>
+						<p>Det blir noe sånn som 1200 kopper i året</p>
+						<p>Det blir noe sånn som mer enn 4 000 000 000 kopper i året(WOH!)><a href="http://www.kaffe.no/vi-drikker-fire-milliarder-kopper-kaffe-i-aret/">[Kilde]</a></p>
+						<p>Ifølge New York Times er Oslo en kjempebra kaffeby! <a class="kilde" href="http://tmagazine.blogs.nytimes.com/2011/10/20/ristretto-coffee-in-oslo/?_r=0">Kilde</a></p>
+						<p>De syns du burde besøke disse stedene:</p>
+						<p><a class="kilde" href="http://www.fuglen.no/">Fuglen</a></p>
+						<p><a class="kilde" href="http://www.javamocca.blogspot.com/">Java espressobar & Kaffeforretning</a></p>
+						<p><a class="kilde" href="http://www.javamocca.blogspot.com/">Mocca Kaffebar & Brenneri</a></p>
+						<p><a class="kilde" href="http://timwendelboe.no/">Tim Wendelboe</a></p>
+					</div>
+				</div>
+			</section>
+
+			<section class="blockWrap" id="bak3" data-speed="100" data-type="background">
+				<div class="block3 block">
+					<div id="oppdagelsen" class="content">
+						<h3>Hvordan oppdaget vi kaffen da?</h3>
+						<p>Det finnes to veldig kjente anekdoter</p>
+						<p>gjeteren som oppdaget at geitene hans ikke sov om natta etter å spist noen frukter</p>
+						<p>Munken som oppdaget</p>
+						<p>Det vi vet derimot</p>
+						<p>Er at fruktene er blitt brukt som oppkvikkere siden 600tallet på afrikas horn</p>
+						<p>og fra ca. 1400 brukt mer som vi kjenner det i dag</p>
+						<p><a href="http://www.kaffeguiden.info/historie.html">[Kilde]</a></p>
+						<p><a href="http://coffeetea.about.com/od/advancedcoffee/a/The-Origin-Of-Coffee.htm">[Kilde]</a></p>
+					</div>
+				</div>	
+			</section>
+
+
+			<section class="blockWrap" id="bak4" data-speed="100" data-type="background">
+				<div class="block4 block">
+					<div id="helse" class="content">
+						<h3>Forresten;</h3>
+						<p>Hvor mange kopper har du drukket i dag?</p>
+						<p>1?</p>
+						<p>2?</p>
+						<p>3?</p>
+						<p>5?</p>
+						<p>Godt, for er du på den 5. koppen så har du senket mulighetene for diabetes type 2 med 50%</p>
+						<p>Du har senket sjansen for å få en masse hjerte og kar sykdommer</p>
+						<p>Du utvikler demens senere enn andre</p>
+						<p>Og mer kaffe du drikker, jo mindre sjanse for parkinson</p>
+						<p>Og skal du trene nå snart?</p>
+						<p>Da kan kaffen hjelpe deg med å øke yteevnen med så mye som 10% </p>
+						<p><a class="kilde" href="http://www.forskning.no/artikler/2006/mai/1148455804.23">Kilde</a></p>
+
+						<h3>Norge er forøvrig nummer 2 på kaffetoppen</h3>
+						<p>Ettefulgt av Finland.</p>
+						<p>Topp 10 listen er som følger</p>
+						<ol>
+							<li>Finland</li>
+							<li>Norge</li>
+							<li>Danmark</li>
+							<li>Nederland</li>
+							<li>Sverige</li>
+							<li>Sveits</li>
+							<li>Beliga</li>
+							<li>Tyskland</li>
+							<li>Canda</li>
+							<li>Estland</li>
+						</ol>
+						<p><a class="kilde" href="http://www.worldmapper.org/posters/worldmapper_1038_coffee_consumption_ver2.pdf">[Kilde]</a></p>
+					</div>
+				</div>
+			</section>
+
+
+			<section class="blockWrap" id="bak5" data-speed="100" data-type="background">
+				<div class="block5 block">
+					<div id="koffein" class="content">
+						<h3>Koffeinen</h3>
+						<p>Ikke bare er den oppkvikkende</p>
+						<p>Den hjelper deg å stimulere galleblærefunksjonen og kan forebygge gallestein</p>
+						<p>Bare ved å drikke kaffe</p>
+						<p>Rekorden på mest på kortest tid</p>
+						<p>- er 82 kopper på 100 timer</p>
+						<p>Og om du er du redd for å drikke for mye kaffe?</p>
+						<p>Vit at det skal 100 kopper til for å drepe deg</p>
+						<p>Og siden kaffe stammer fra frukt</p>
+						<p>Kan vi da si at det er en av de fem om dagen?</p>
+						<p>Kaffe er 60% av antioksidant inntaket på den norske befolkningen</p>
+						<p><a href="http://www.huffingtonpost.com/2013/08/25/caffeine-facts_n_3814825.html">[Kilde]</a></p>
+						<p><a href="http://en.ilovecoffee.jp/posts/view/92">[Kilde]</a></p>
+					</div>
+				</div>
+			</section>
+
+			<section class="blockWrap" id="bak6" data-speed="100" data-type="background">
+				<div class="block6 block">
+					<div id="hvordankaffen" class="content">
+						<h3>Hvordan tar du kaffen din?</h3>
+						<p>Svart?</p>
+						<p>Med melk?</p>
+						<p>Som latte?</p>
+						<p>Fra lokale kaffebaren?</p>
+						<p>Har du en kopp kaffe nå?</p>
+						<p>Ikke? Hent en nå, jeg venterher så lenge</p>
+						<p>Ordnet?</p>
+						<p>Smaker den ikke godt?</p>
+						<p>Det sies at lukten av kaffe i seg selv har en helsebringende effekt</p>
+						<p>Lukten av velvære</p>
+						<p>Har du prøvd en red eye - filterkaffe med en shot espresso</p>
+						<p>Viatnemesisk iskaffe?</p>
+						<p>Og hvordan lager du den?</p>
+						<p>En kapsel?</p>
+						<p>Fra Moccamaster?</p>
+						<p>Presskanne?</p>
+						<p>Tilberedt med en chemex?</p>
+						<p>Slow-drip - som tar 12 timer</p>
+						<p>Når kaffe eksporteres er den lysegrønn nesten hvit</p>
+						<p>Så brennes den i Norge</p>
+						<p>Brenningen avgjør mye av smaken</p>
+						<p>Mye brent gir XXXX</p>
+						<p>Lite brent XXX</p>
+						<p>Og det kan legges til aroma</p>
+						<p>Så kvernes kaffen</p>
+						<p>Enten hos kaffehuset</p>
+						<p>Eller kverner du bønnene selv?</p>
+					</div>
+				</div>
+			</section>
+
+			<section class="blockWrap" = id="kaffeV">
+				<div id="kaffevelger">
+					<?php include('pages/kaffevelger.php'); ?>
+				</div>
+			</section>
+
+			<section class="blockWrap" id="bak7" data-speed="100" data-type="background">
+				<div class="block7 block">
+					<div id="merkoffein" class="content">
+						<h3>Kjenner du deg kvikk?</h3>
+						<p>Selvfølgelig gjør du det</p>
+						<p>For du hentet kaffe sant?</p>
+						<p>Koffeinen tas opp nesten umiddelbart</p>
+						<p>En kopp kaffe inneholder opptil 200mg koffein</p>
+						<p>Energidrikker? Ca. 80mg. </p>
+						<p>Og en stor kaffe fra Starbucks har like mye koffein som 9.5 bokser cola</p>
+						<p><a href="http://www.energyfiend.com/caffeine-content/coffee-brewed">Kilde</a></p>
+						<p>Og er kaffe din lysbrent inneholder den mer koffein enn om den er  mørkbrent </p>
+						<p>Nyt kaffen din</p>
+						<p>Du har glede av den</p>
+						<p>Mentalt</p>
+						<p>Og fysisk</p>
+						<p>Man finner forøvrig koffein i mer enn 60 planter</p>
+						<p>Og 90% av jordas befolkning tar til seg koffein på et vis</p>
+						<p>Nyter du en bit sjokolade til kaffen din?</p>
+						<p>Den inneholder også koffein</p>
+						<p>Og smertestillende kan også inneholde koffein</p>
+						<p>Gjør de det er de inntil 40% mer effektive</p>
+						<p>og kan hjelpe kroppen til å ta det opp raskere</p>
+						<p>Har du opplevd å få fine mønster i kaffen din?</p>
+						<p>Har du opplevd å få en figur i kaffen din?</p>
+						<p>Kaffebarer har lenge servert cappuccino med et hjerte i toppen</p>
+						<p>Nå skal det være 3d-figurer i skummet</p>
+						<p>Hva blir det neste?</p>
+					</div>
+				</div>
+			</section>
+
+			<section class="blockWrap" id="bak8" data-speed="100" data-type="background">
+				<div class="block8 block">
+		 			<div id="ordetkaffe" class="content">
+		 				<h3>Kaffe - kaffe. Smak på ordet</h3>
+		 				<p>Hvor kommer det fra?</p>
+		 				<p>Første gang dokumentert er fra 1598 på italiensk, caffe</p>
+		 				<p>Som igjen er fra tyrkisk kahve</p>
+		 				<p>Som igjen er avledet fra det arabiske ordet qahwah (kaffe). </p>
+		 				<p>Det siste skal etter sigende bety vin, </p>
+		 				<p>Mer sannsynlig har det sin opprinnelse fra den etiopiske regionen Kaffa, et tradisjonelt område for kaffeproduksjon.</p>
+		 				<p>Og ordet café - ja du har alltid antatt det riktige. Det sxtammer fra ordet kaffe <a class="kilde" href="http://snl.no/kaf%C3%A9">[Kilde]</a></p>
+						<p>Kaffe er forøvrig kalt buno i Etiopia</p>
+		 				<p><a class="kilde"  href="http://www.etymonline.com/index.php?search=coffee&amp;searchmode=none">[Kilde]</a></p>
+					</div>
+				</div>
+			</section>
+
+
+			<section class="blockWrap" id="bak9" data-speed="100" data-type="background">
+				<div class="block9 block">
+							<div id="prodland" class="content">
+								<h3>Etiopia</h3>
+								<p>Etiopa. Hvor kaffe kom fra, og fortsatt kommer fra</p>
+								<p>Er i dag det 5. mest produserende landet</p>
+								<p>Topp 10 er forøvrig</p>
+								<p>1/3 av kaffen kommer faktisk fra Brasil.</p>
+								<ol>
+									<li>Brazil</li>
+									<li>Vietnam</li>
+									<li>Elfenbenskysten</li>
+									<li>Indonesia</li>
+									<li>Etiopia</li>
+									<li>India</li>
+									<li>Mexico</li>
+									<li>Guatemala</li>
+									<li>Peru</li>
+									<li>Honduras</li>
+								</ol>
+								<p><a class="kilde" href="http://www.fas.usda.gov/htp/coffee/2009/December_2009/2009_coffee_december.pdf">Kilde</a></p>
+								<p>Har du forresten prøvd cà phê đá?</p>
+								<p>Eller vietnamesisk iskaffe som vi kaller, det kan gjøres med å lage vietnamesisk kaffe rett i et glass med isbiter, og så tilsetter man kondensert melk</p>
+								<p>(og man kan tilsette alkohol, f.eks. whisky)</p>
+								<p>Apropos, whisky. Hadde vært gøy å koblet whisky og kaffe. Det må vi få til.</p>
+						</div>
+				</div>
+				
+			</section>
+
+
+			<section class="blockWrap" id="bak10" data-speed="100" data-type="background">
+				<div class="block10 block">
+					<div id="kaffeogsamfunn" class="content">
+						<h3>Kaffe og samfunn</h3>
+						<p>Kaffe er i dag en del av samfunnet</p>
+						<p>Kaffe er overalt der vi ferdes</p>
+						<p>Man møter venner over en kopp kaffe</p>
+						<p>Det er aldri et møte uten kaffe</p>
+						<p>Man har kirkekaffe</p>
+					</div>
+				</div>
+			</section>
+
+
+			<section class="blockWrap" id="bak11" data-speed="100" data-type="background">
+				<div class="block11 block">
+					<div id="kaffeansvar" class="content">
+						<h3>Fra kaffen såes i jorda</h3>	
+						<p>- til den er i din kopp</p>
+						<p>Trengs det 140 liter vann</p>
+						<p>Kaffe dyrkes i land med allerede dårlig tilgang på ferskvann som Etiopia og Kenya</p>
+						<p>Tettbefolkede land som India og Kina har en oppvoksende generasjon som har begynt å få smaken for kaffe</p>
+						<p>Det sies at verden trenger 25-30millioner flere kaffesekker i 2020. </p>
+						<p>Ved hjelp av kunnskapsutveksling og investeringer i moderne produksjonsmetoder og infrastruktur kan en kaffebonde så mye som tidoble produksjonen sin</p>
+						<p>For å dekke verdens behov må kaffen bli dyrere</p>
+						<p>Så kaffebønder kan investere i det utstyret og kunnskapen som trengs for å effektivisere produksjonen sin</p>
+						<p><a class="kilde" href="http://www.nationen.no/2013/09/16/naring/dagens_kommentar/kommentar/kaffe/fairtrade/8274215/">[Kilde]</a></p>
+						<p><a class="kilde" href="http://www.kaffe.no/verden-trenger-mer-kaffe-til-l%C3%B8nnsomme-priser/">[Kilde]</a></p>
+						<p><a class="kilde" href="http://coffeelands.crs.org/2012/09/302-the-water-footprint-of-your-coffee/">[Kilde]</a></p>
+					</div>
+				</div>
+			</section>
+			<section class="blockWrap" id="bak12" data-speed="100" data-type="background">
+
+			</section>
+
+	</body>
+</html>
