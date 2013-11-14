6854fc4d083c59179c4f0f93a56cd77c45657300
kristofferosen <kristofferosen@gmail.com>
2013-11-14 20:05:49 +0100

    INNHOLD Jeg har vært streng og endre på masse

--- www/wwwMASTER/indexrevurderingavtekst.php
+++ www/wwwMASTER/indexrevurderingavtekst.php
@@ -50,13 +50,12 @@
 			</section>
 
 
-
 			<section class="blockWrap" id="bak2" data-speed="100" data-type="background">
 				<div class="block2 block">
 					<div id="kaffetilnorge" class="content">
 						<h3>Enn veien til Norge da</h3>
 						<p>Norge fikk kaffe på 1600 tallet</p>
-						<p>OG da var det en luksusvare, for rikfolket</p>
+						<p>Og da var det en luksusvare, for rikfolket</p>
 						<p>I dag drikkes det heldigvis av alle sosiale lag</p>
 						<p>I dag drikker vi 3-4 kopper om dagen hver</p>
 						<p>Det blir noe sånn som 1200 kopper i året</p>
@@ -90,64 +89,6 @@
 
 			<section class="blockWrap" id="bak4" data-speed="100" data-type="background">
 				<div class="block4 block">
-					<div id="helse" class="content">
-						<h3>Forresten;</h3>
-						<p>Hvor mange kopper har du drukket i dag?</p>
-						<p>1?</p>
-						<p>2?</p>
-						<p>3?</p>
-						<p>5?</p>
-						<p>Godt, for er du på den 5. koppen så har du senket mulighetene for diabetes type 2 med 50%</p>
-						<p>Du har senket sjansen for å få en masse hjerte og kar sykdommer</p>
-						<p>Du utvikler demens senere enn andre</p>
-						<p>Og mer kaffe du drikker, jo mindre sjanse for parkinson</p>
-						<p>Og skal du trene nå snart?</p>
-						<p>Da kan kaffen hjelpe deg med å øke yteevnen med så mye som 10% </p>
-						<p><a class="kilde" href="http://www.forskning.no/artikler/2006/mai/1148455804.23">Kilde</a></p>
-
-						<h3>Norge er forøvrig nummer 2 på kaffetoppen</h3>
-						<p>Ettefulgt av Finland.</p>
-						<p>Topp 10 listen er som følger</p>
-						<ol>
-							<li>Finland</li>
-							<li>Norge</li>
-							<li>Danmark</li>
-							<li>Nederland</li>
-							<li>Sverige</li>
-							<li>Sveits</li>
-							<li>Beliga</li>
-							<li>Tyskland</li>
-							<li>Canda</li>
-							<li>Estland</li>
-						</ol>
-						<p><a class="kilde" href="http://www.worldmapper.org/posters/worldmapper_1038_coffee_consumption_ver2.pdf">[Kilde]</a></p>
-					</div>
-				</div>
-			</section>
-
-
-			<section class="blockWrap" id="bak5" data-speed="100" data-type="background">
-				<div class="block5 block">
-					<div id="koffein" class="content">
-						<h3>Koffeinen</h3>
-						<p>Ikke bare er den oppkvikkende</p>
-						<p>Den hjelper deg å stimulere galleblærefunksjonen og kan forebygge gallestein</p>
-						<p>Bare ved å drikke kaffe</p>
-						<p>Rekorden på mest på kortest tid</p>
-						<p>- er 82 kopper på 100 timer</p>
-						<p>Og om du er du redd for å drikke for mye kaffe?</p>
-						<p>Vit at det skal 100 kopper til for å drepe deg</p>
-						<p>Og siden kaffe stammer fra frukt</p>
-						<p>Kan vi da si at det er en av de fem om dagen?</p>
-						<p>Kaffe er 60% av antioksidant inntaket på den norske befolkningen</p>
-						<p><a href="http://www.huffingtonpost.com/2013/08/25/caffeine-facts_n_3814825.html">[Kilde]</a></p>
-						<p><a href="http://en.ilovecoffee.jp/posts/view/92">[Kilde]</a></p>
-					</div>
-				</div>
-			</section>
-
-			<section class="blockWrap" id="bak6" data-speed="100" data-type="background">
-				<div class="block6 block">
 					<div id="hvordankaffen" class="content">
 						<h3>Hvordan tar du kaffen din?</h3>
 						<p>Svart?</p>
@@ -187,6 +128,63 @@
 				</div>
 			</section>
 
+
+			<section class="blockWrap" id="bak5" data-speed="100" data-type="background">
+				<div class="block5 block">
+					<div id="helse" class="content">
+						<h3>Forresten;</h3>
+						<p>Hvor mange kopper har du drukket i dag?</p>
+						<p>1?</p>
+						<p>2?</p>
+						<p>3?</p>
+						<p>5?</p>
+						<p>Godt, for er du på den 5. koppen, så har du senket mulighetene for diabetes type 2 med 50%!</p>
+						<p>Du har senket sjansen for å få en masse hjerte og kar sykdommer!</p>
+						<p>Du utvikler demens senere enn andre!</p>
+						<p>Og mer kaffe du drikker, jo mindre sjanse for parkinson!</p>
+						<p>Og siden kaffe stammer fra frukt</p>
+						<p>Kan vi da si at det er en av de fem om dagen?</p>
+
+						<h3>Norge er forøvrig nummer 2 på kaffetoppen</h3>
+						<p>Ettefulgt av Finland.</p>
+						<p>Topp 10 listen er som følger</p>
+						<ol>
+							<li>Finland</li>
+							<li>Norge</li>
+							<li>Danmark</li>
+							<li>Nederland</li>
+							<li>Sverige</li>
+							<li>Sveits</li>
+							<li>Beliga</li>
+							<li>Tyskland</li>
+							<li>Canda</li>
+							<li>Estland</li>
+						</ol>
+						<p><a class="kilde" href="http://www.worldmapper.org/posters/worldmapper_1038_coffee_consumption_ver2.pdf">[Kilde]</a></p>
+					</div>
+				</div>
+			</section>
+
+
+			<section class="blockWrap" id="bak6" data-speed="100" data-type="background">
+				<div class="block6 block">
+					<div id="koffein" class="content">
+						<h3>Koffeinen</h3>
+						<p>Ikke bare er den oppkvikkende</p>
+						<p>Den hjelper deg å stimulere galleblærefunksjonen og kan forebygge gallestein</p>
+						<p>Rekorden på mest kaffe på kortest tid</p>
+						<p>- er 82 kopper på 100 timer</p>
+						<p>Og om du er du redd for å drikke for mye kaffe?</p>
+						<p>Vit at det skal 100 kopper til for å drepe deg</p>
+						
+						<p><a href="http://www.huffingtonpost.com/2013/08/25/caffeine-facts_n_3814825.html">[Kilde]</a></p>
+						<p><a href="http://en.ilovecoffee.jp/posts/view/92">[Kilde]</a></p>
+					</div>
+				</div>
+			</section>
+
+
+
 			<section class="blockWrap" id="bak7" data-speed="100" data-type="background">
 				<div class="block7 block">
 					<div id="merkoffein" class="content">
@@ -196,25 +194,17 @@
 						<p>Koffeinen tas opp nesten umiddelbart</p>
 						<p>En kopp kaffe inneholder opptil 200mg koffein</p>
 						<p>Energidrikker? Ca. 80mg. </p>
-						<p>Og en stor kaffe fra Starbucks har like mye koffein som 9.5 bokser cola</p>
-						<p><a href="http://www.energyfiend.com/caffeine-content/coffee-brewed">Kilde</a></p>
+						<p>Og en stor kaffe fra Starbucks har like mye koffein som 9.5 bokser cola(<a href="http://www.energyfiend.com/caffeine-content/coffee-brewed">Kilde</a>)</p>
 						<p>Og er kaffe din lysbrent inneholder den mer koffein enn om den er  mørkbrent </p>
-						<p>Nyt kaffen din</p>
+						<p>Nyt kaffen din!</p>
 						<p>Du har glede av den</p>
 						<p>Mentalt</p>
 						<p>Og fysisk</p>
-						<p>Man finner forøvrig koffein i mer enn 60 planter</p>
-						<p>Og 90% av jordas befolkning tar til seg koffein på et vis</p>
 						<p>Nyter du en bit sjokolade til kaffen din?</p>
 						<p>Den inneholder også koffein</p>
 						<p>Og smertestillende kan også inneholde koffein</p>
 						<p>Gjør de det er de inntil 40% mer effektive</p>
 						<p>og kan hjelpe kroppen til å ta det opp raskere</p>
-						<p>Har du opplevd å få fine mønster i kaffen din?</p>
-						<p>Har du opplevd å få en figur i kaffen din?</p>
-						<p>Kaffebarer har lenge servert cappuccino med et hjerte i toppen</p>
-						<p>Nå skal det være 3d-figurer i skummet</p>
-						<p>Hva blir det neste?</p>
 					</div>
 				</div>
 			</section>
@@ -261,29 +251,17 @@
 								<p>Har du forresten prøvd cà phê đá?</p>
 								<p>Eller vietnamesisk iskaffe som vi kaller, det kan gjøres med å lage vietnamesisk kaffe rett i et glass med isbiter, og så tilsetter man kondensert melk</p>
 								<p>(og man kan tilsette alkohol, f.eks. whisky)</p>
-								<p>Apropos, whisky. Hadde vært gøy å koblet whisky og kaffe. Det må vi få til.</p>
+								<p>HAR DU NOEN GANG DRUKKET EN KAFFEKATT?</p>
+								<p>INsert bilde av 3d foam kaffekatt her</p>
 						</div>
 				</div>
 				
 			</section>
 
 
+
 			<section class="blockWrap" id="bak10" data-speed="100" data-type="background">
 				<div class="block10 block">
-					<div id="kaffeogsamfunn" class="content">
-						<h3>Kaffe og samfunn</h3>
-						<p>Kaffe er i dag en del av samfunnet</p>
-						<p>Kaffe er overalt der vi ferdes</p>
-						<p>Man møter venner over en kopp kaffe</p>
-						<p>Det er aldri et møte uten kaffe</p>
-						<p>Man har kirkekaffe</p>
-					</div>
-				</div>
-			</section>
-
-
-			<section class="blockWrap" id="bak11" data-speed="100" data-type="background">
-				<div class="block11 block">
 					<div id="kaffeansvar" class="content">
 						<h3>Fra kaffen såes i jorda</h3>	
 						<p>- til den er i din kopp</p>
@@ -300,7 +278,7 @@
 					</div>
 				</div>
 			</section>
-			<section class="blockWrap" id="bak12" data-speed="100" data-type="background">
+			<section class="blockWrap" id="bak11" data-speed="100" data-type="background">
 
 			</section>
 
