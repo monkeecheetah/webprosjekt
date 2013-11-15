38fa93a98253ef8dc7855e6a3c47788ba058591c
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-15 11:47:15 +0100

    Laget function blockFade()

--- www/wwwkris/js/script.js
+++ www/wwwkris/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	// Cache the Window object
+	blockFade();
+	function blockFade(){ 
+	 $('.block0').fadeTo(1000,1, function(){
+	 	$(this).fadeTo(1000,0, function(){
+	 		blockFade();
+	 	});
+	 })
+	};
 	
 	$window = $(window);
 	$('section[data-type="background"]').each(function(){
@@ -25,13 +33,14 @@ tiles = $("#intro h3, #intro p").fadeTo(0,0);
    		$(this).css('margin-left', counter+'px');
    		counter += 20;
    	});
-
-
 }); // end of Document on ready
+
+
+
 $(window).scroll(function(d,h) {
     tiles.each(function(i) {
         a = $(this).offset().top + $(this).height();
         b = $(window).scrollTop() + $(window).height();
-        if (a < b) $(this).fadeTo(500,1);
+        if (a < b) $(this).fadeTo(1000,1);
     });
 });
