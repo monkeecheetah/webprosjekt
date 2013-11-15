c6025c7e9ea2a3cef388935a075b330afe31ff56
kristofferosen <kristofferosen@gmail.com>
2013-11-15 17:29:53 +0100

    Kaffetilnorge plasserer og fikser

--- www/wwwMASTER/indexrevurderingavtekst.php
+++ www/wwwMASTER/indexrevurderingavtekst.php
@@ -54,7 +54,7 @@
 			<section class="blockWrap" id="bak2" data-speed="100" data-type="background">
 				<div class="block2 block">
 					<div id="kaffetilnorge" class="content">
-						<h3>HVORDAN KOM KAFFEN TIL NORGE?</h3>
+						<h3>KAFFE TIL NORGE?</h3>
 						<p>Norge fikk kaffe på 1600 tallet</p>
 						<p>Og da var det en luksusvare, for rikfolket</p>
 						<p>I dag drikkes det heldigvis av alle sosiale lag</p>
