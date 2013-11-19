684ded0fa84d2a34d29cdc66240eb4e97e6745c8
kristofferosen <kristofferosen@gmail.com>
2013-11-19 11:37:43 +0100

    prøver mer med bakgrunnsbildet

--- www/wwwMASTER/index.php
+++ www/wwwMASTER/index.php
@@ -128,10 +128,12 @@
 				</div>
 			</section>
 
-			<section class="blockWrap" id="kaffeV">
-				<div id="kaffevelger">
-					<h3 id="kaffevelgerheader">KAFFEVELGER</h3>
-					<?php include('pages/kaffevelger.php'); ?>
+			<section class="blockWrap" id="kaffeV" data-speed="100" data-type="background">
+				<div id="blockvelger block">
+					<div id="kaffevelger">
+						<h3 id="kaffevelgerheader">KAFFEVELGER</h3>
+						<?php include('pages/kaffevelger.php'); ?>
+					</div>
 				</div>
 			</section>
 
