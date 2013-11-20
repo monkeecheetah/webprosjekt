feaf87bc80be232b7df36a8196f9ac1ced5d95d2
chrban <mac@dhcp-158-39-196-138.wlan.hioa.no>
2013-11-20 14:53:00 +0100

    lagt til første møtereferat

--- www/prosjektrapport/pages/møtereferater.php
+++ www/prosjektrapport/pages/møtereferater.php
@@ -1,1 +1,24 @@
-<h1>vøøøbøvøvøbmøøter</h1>
+<h1>Møtereferat</h1>
+<h3>Onsdag 23.10.2013</h3>
+	<p>
+		<ul>
+			<li>Møttes for første gang, ble kjent.</li>
+			<li>Oprettet gruppe på facebook, mappe på drive og repository på github.</li>
+			<li>Forventningsavklaring:</li>
+				<ul>
+					<li>At alle drar lasset sammen.</li>
+					<li>At alle får bidratt med det de kan, samt blir utfordret på ny teknologier.</li>
+				</ul>
+			<li>Ambisjonsnivå</li>
+				<ul><li>Karakter A</li></ul>
+			<li>Strategi fo godt resultat:</li>
+				<ul><li>Å bruke det vi har lært i pensum kombinert med presentasjon av et tema som motiverer alle til jevnt og godt arbeid.</li></ul>
+			<li>Hvem kan hva:</li>
+				<ul><li>Kristoffer kan definitivt mest, og resten av gruppen er komfortable med html, css og litt javascript/php. Osen er flink med photoshop.</li></ul>
+			<li>Plan til neste møte:</li>
+				<ul><li>Tenke hvilken tema vi ønsker å lage oppgave om. Hva vil du jobbe med? Hva vil du ikke jobbe med?</li></ul>
+			<li>Fellers interesser:</li>
+				<ul><li>Musikk, kunst og kaffe</li></ul>
+
+
+		</ul>
