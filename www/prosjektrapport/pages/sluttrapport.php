1f9dc190b5042b256ea53b1c9143f341ffe782a7
kristofferosen <kristofferosen@gmail.com>
2013-11-20 17:30:19 +0100

    meeer ting sluttrapportenerererne

--- www/prosjektrapport/pages/sluttrapport.php
+++ www/prosjektrapport/pages/sluttrapport.php
@@ -4,47 +4,45 @@
 <h3>Innhold</h3>
 <a href="#innledning">1. Innledning</a>
 <ul>
-	<li><a href="#problemstilling">Problemstilling</a></li>
-	<li><a href="#mål">Mål</a></li>
-	<li><a href="#ambisjonsnivå">Ambisjonsnivå</a></li>
+	<li><a href="#problemstilling">1.1 Problemstilling</a></li>
+	<li><a href="#mål">1.2 Mål</a></li>
+	<li><a href="#ambisjonsnivå">1.3 Ambisjonsnivå</a></li>
 </ul>
 
 
 <a href="#rammer">2. Rammer</a>
 <ul>
-	<li><a href="#teknologi">Teknologi</a></li>
-	<li><a href="#målgruppe">Målgruppe</a></li>
-	<li><a href="#arbeidsmetode">Arbeidsmetode</a></li>
-	<li><a href="#roller">Roller</a></li>
+	<li><a href="#teknologi">2.1 Teknologi</a></li>
+	<li><a href="#målgruppe">2.2 Målgruppe</a></li>
+	<li><a href="#arbeidsmetode">2.3 Arbeidsmetode</a></li>
+	<li><a href="#roller">2.4 Roller</a></li>
 </ul>
 
 
 <a href="#prosessen">3. Utviklingsprosess</a>
 <ul>
-	<li><a href="#stegforsteg">Steg for steg</a></li>
-	<li><a href="#teknologiskutvikling">Teknologisk utvikling</a></li>
-	<li><a href="#tegning">Produksjon av egne bilder</a></li>
+	<li><a href="#stegforsteg">3.1 Steg for steg</a></li>
+	<li><a href="#teknologiskutvikling">3.2 Teknologisk utvikling</a></li>
+	<li><a href="#tegning">3.3 Produksjon av egne bilder</a></li>
 </ul>
 
 
 
 <a href="#resultat">4. Resultat</a>
 <ul>
-	<li><a href="#presentasjon">Presentasjon av nettside</a></li>
-	<li><a href="#scroll">Scrollefunksjon</a></li>
-	<li><a href="#kaffevelger">Kaffevelger</a></li>
-	<li><a href="#design">Design</a></li>
-	<li><a href="#bilder">Bilder</a></li>
-	<li><a href="#responsivt">Responsivt design</a></li>
+	<li><a href="#presentasjon">4.1 Presentasjon av nettside</a></li>
+	<li><a href="#scroll">4.2 Scrollefunksjon</a></li>
+	<li><a href="#kaffevelger">4.3 Kaffevelger</a></li>
+	<li><a href="#design">4.4 Design</a></li>
+	<li><a href="#bilder">4.5 Bilder</a></li>
+	<li><a href="#responsivt">4.6 Responsivt design</a></li>
 
 </ul>
 
 <a href="#valg">5. Valg</a>
 <ul>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
+	<li><a href="wcag">5.1 WCAG</a></li>
+	<li><a href="dynamikk"> 5.2Dynamikk</a></li>
 </ul>
 
 <a href="#evaluering">6. Evaluering</a>
@@ -84,16 +82,16 @@
 	<h2>1. Innledning</h2>
 
 	<div id="problemstilling">
-		<h3>Problemstilling</h3>
+		<h3>1.1 Problemstilling</h3>
 		<p><em>Kan vi ved hjelp av en nettsides virkemidler inspirere en kaffedrikkers forhold til kaffe?</em></p>
 	</div>
 
 	<div id="mål">
-		<h3>Mål</h3>
+		<h3>1.2 Mål</h3>
 	</div>
 
 	<div id="abisjonsnivå">
-		<h3>Ambisjonsnivå</h3>
+		<h3>1.3 Ambisjonsnivå</h3>
 		<p>På første møte ble det klart alt alle hadde et høyt ambisjonsnivå for prosjektet.
 		Vi bestemte oss for at resulatet vårt skulle være godt nok til å få en A. På spørreundersøkelsen
 		pre-gruppeinndeling hadde alle krysset av på å sette av 8 timer i uken til prosjektet.
@@ -106,16 +104,16 @@
 <section id="rammer">
 	<h2>2. Rammer</h2>
 	<div id ="teknologi">
-		<h3>Teknologi</h3>
+		<h3>2.1 Teknologi</h3>
 	</div>
 
 	<div id="målgruppe">
-		<h3>Målgruppe</h3>
+		<h3>2.2 Målgruppe</h3>
 		<p><em>Typisk norsk kaffedrikker som bare ikke enda vet at han/hun ønsker å lære mer om kaffe.</em></p>
 	</div>
 
 	<div id="arbeidsmetode">
-		<h3>Arbeidsmetode</h3>
+		<h3>2.3 Arbeidsmetode</h3>
 		<p>En blanding av scrumteknikker og prototyping. Scrum virker ikke veldig egnet for et såpass lite prosjekt med så få medlemmer.
 		Særlig siden ingen av oss har erfaring med det fra før av. Men vi tar med oss sykluser fra scrum og bruker de som milepæler.
 		I tillegg skal vi høy møtefrekvens hvor vi møtes og diskuterer utfordringer og staker ut veien videre hvor det skrives møtereferater
@@ -124,7 +122,7 @@
 	</div>
 
 	<div id="roller">
-		<h3>Roller</h3>
+		<h3>2.4 Roller</h3>
 		<ul>
 			<li>Kristoffer Osen: Gruppeleder</li>
 			<li>Hege Grostad: Innhold- og designansvarlig</li>
@@ -139,18 +137,18 @@
 	<h2>3. Utviklingsprosessen</h2>
 
 	<div id="steg">
-		<h3>Steg for steg</h3>
+		<h3>3.1 Steg for steg</h3>
 		<p>Forklaring av prosessen ved hjelp av milepæler(Går vekk fra jarallax, finner substitutt,
 		 design tar form, utviklig av innhold og dramaturgi, finn eksempler i møtereferater)</p>
 	</div>
 
 	<div id="teknologiskutvikling">
-		<h3>Teknologisk utvikling</h3>
+		<h3>3.2 Teknologisk utvikling</h3>
 		<p>Grundig forklaring av hvorfor ikke jarallax funket, hvordan vi løste problemet og hva vi byttet jarallax for.</p>
 	</div>
 
 	<div id="tegning">
-		<h3>Produksjon av bilder</h3>
+		<h3>3.3 Produksjon av bilder</h3>
 		<p>Hvordan var det å tegne bilder, Hege?</p>
 	</div>
 
@@ -169,33 +167,33 @@
 <section id="resultat">
 	<h2>4. Resultat</h2>
 	<div id="presentasjon">
-		<h3>Presentasjon av nettsiden</h3>
+		<h3>4.1 Presentasjon av nettsiden</h3>
 		<p>Forklare sidens grunnprinsipp og forklare nettsiden side for side</p>
 	</div>
 
 	<div id="scroll">
-		<h3>Scrollefunksjon</h3>
+		<h3>4.2 Scrollefunksjon</h3>
 		<p>Forklare hvordan vi har laget scrollingen</p>
 	</div>
 
 	<div id="kaffevelger">
-		<h3>Kaffevelger</h3>
+		<h3>4.3 Kaffevelger</h3>
 		<p>Forklare hvordan kaffevelgeren funker</p>
 	</div>
 
 	<div id="design">
-		<h3>Design</h3>
+		<h3>4.4 Design</h3>
 		<p>Generelt om konsept(enkle, stilrene bilder, tekst over. Fortelle hvordan vi har valgt å legge tekst, fargevalg, font og tekststørrelse. </p>
 	</div>
 
 	<div id="bilder">
-		<h3>Bilder</h3>
+		<h3>4.5 Bilder</h3>
 		<p>Fortelle om bildene vi har valg, hvorfor de er valgt og hva vi har gjort med de. Litt om hege sine bilder</p>
 	</div>
 
 
 	<div id="responsivt">
-		<h3>Responsivt design</h3>
+		<h3>4.6 Responsivt design</h3>
 		<p>Hva har vi gjort for at nettsiden skal funke på alle sjermer, i alle browsere</p>
 	</div>
 	Presentasjon av resultat(Beskrivelse av oppbyggning, scrollefuksjon og  fadin og kaffevelgeren(Beskrivelse av Jquerykode), produksjon av egne bilder, beskrivelse av design, plassering av tekst, font, bakgrunsbilder, 
@@ -206,6 +204,17 @@
 
 <section id="valg">
 	<h2>5. Valg</h2>
+	<div id="wcag">
+		<h3>5.1 WCAG</h3>
+		<p>wcag? what the fuck is wcag?</p>
+	</div>
+
+	<div id="dynamikk">
+		<h3>5.2 Dynamikk</h3>
+		<p>Begrunne hvorfor siden er bygd opp som den er. Hvorfor har vi fet scrollefunksjon? 
+			Hvorfor fancy bilder? Hvorfor enkel layout? JOOOO fordi, siden skal være spennende 
+			å lese og brukers nysjerrighet skal trigges og han/hun/den skal bli INSPIRERT til å drikke mer kaffe!!! </p>
+	</div>
 
 	Valg (hovedsakelig anngående WCAG, begrunnelse for scrollefunksjon og sideoppbyggning samt layout.)
 
