34dd78ec3f70846fd6ce89fc4e22f89e9aad1d78
kristofferosen <kristofferosen@gmail.com>
2013-11-20 16:10:53 +0100

    endrer overskrifter på sluttrapport

--- www/prosjektrapport/pages/sluttrapport.php
+++ www/prosjektrapport/pages/sluttrapport.php
@@ -43,18 +43,19 @@
 
 
 <section id="innledning">
-	<h3>Innledning</h3>
+	<h2>1. Innledning</h2>
 
 	<div id="problemstilling">
-		<h2>Problemstilling</h2>
+		<h3>Problemstilling</h3>
 		<p><em>Kan vi ved hjelp av en nettsides virkemidler inspirere en kaffedrikkers forhold til kaffe?</em></p>
 	</div>
 
 	<div id="mål">
-		<h2>Mål</h2>
+		<h3>Mål</h3>
 	</div>
 
 	<div id="abisjonsnivå">
+		<h3>Ambisjonsnivå</h3>
 		<p>På første møte ble det klart alt alle hadde et høyt ambisjonsnivå for prosjektet.
 		Vi bestemte oss for at resulatet vårt skulle være godt nok til å få en A. På spørreundersøkelsen
 		pre-gruppeinndeling hadde alle krysset av på å sette av 8 timer i uken til prosjektet.
@@ -65,18 +66,18 @@
 
 
 <section id="rammer">
-	<h3>Rammer</h3>
+	<h2>2. Rammer</h2>
 	<div id ="teknologi">
-		<h2>Teknologi</h2>
+		<h3>Teknologi</h3>
 	</div>
 
 	<div id="målgruppe">
-		<h2>Målgruppe</h2>
+		<h3>Målgruppe</h3>
 		<p><em>Typisk norsk kaffedrikker som bare ikke enda vet at han/hun ønsker å lære mer om kaffe.</em></p>
 	</div>
 
 	<div id="arbeidsmetode">
-		<h2>Arbeidsmetode</h2>
+		<h3>Arbeidsmetode</h3>
 		<p>En blanding av scrumteknikker og prototyping. Scrum virker ikke veldig egnet for et såpass lite prosjekt med så få medlemmer.
 		Særlig siden ingen av oss har erfaring med det fra før av. Men vi tar med oss sykluser fra scrum og bruker de som milepæler.
 		I tillegg skal vi høy møtefrekvens hvor vi møtes og diskuterer utfordringer og staker ut veien videre hvor det skrives møtereferater
@@ -85,7 +86,7 @@
 	</div>
 
 	<div id="roller">
-		<h2>Roller</h2>
+		<h3>Roller</h3>
 		<ul>
 			<li>Kristoffer Osen: Gruppeleder</li>
 			<li>Hege Grostad: Innhold- og designansvarlig</li>
@@ -97,27 +98,27 @@
 
 
 <section id="prosessen">
-	<h3>Utviklingsprosessen</h3>
+	<h2>3. Utviklingsprosessen</h2>
 	Utviklingsprosess(detaljert om metode, vendepunkter, valg, ansvar, forkasting av teknologi, hvilken kaffe har vi drukket,
 
 </section>
 
 
 <section id="resultat">
-	<h3>Resultat</h3>
+	<h2>4. Resultat</h2>
 	Presentasjon av resultat(Beskrivelse av oppbyggning, scrollefuksjon og  fadin og kaffevelgeren(Beskrivelse av Jquerykode), produksjon av egne bilder, beskrivelse av design, plassering av tekst, font, bakgrunsbilder, 
 </section>
 
 
 <section id="valg">
-	<h3>Valg</h3>
+	<h2>5. Valg</h2>
 	Valg (hovedsakelig anngående WCAG, begrunnelse for scrollefunksjon og sideoppbyggning samt layout.)
 
 </section>
 
 
 <section id="evaluering">
-	<h3>Evaluering</h3>
+	<h2>6. Evaluering</h2>
 	Evaluering(av metode, valg, teknologier, 
 
 
@@ -125,7 +126,7 @@
 
 
 <section id="refleksjon">
-	<h3>Refleksjon</h3>
+	<h2>7. Refleksjon</h2>
 	Refleksjon (rundt ansvar, samarbeid, ambisjonsnivå, sluttprodukt, 
 
 
@@ -133,7 +134,7 @@
 
 
 <section id="konklusjon">
-	<h3>Konklusjon</h3>
+	<h2>8. Konklusjon</h2>
 	Konklusjoner(hva funket/hva funket ikke, hva har vi lært, hva sitter vi igjen med
 
 
@@ -141,7 +142,7 @@
 
 
 <section id="vedlegg">
-	<h3>Litteraturhenvisninger og begrepsforklaringer</h3>
+	<h2>9. Litteraturhenvisninger og begrepsforklaringer</h2>
 	Litteraturhenvisninger og begrepsforklaringer ???
 
 </section>
