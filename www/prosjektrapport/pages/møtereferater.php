7c91f00cde2f69e1c7149ef9a0f4dc78a3b8b414
chrban <mac@dhcp-158-39-196-138.wlan.hioa.no>
2013-11-20 15:11:22 +0100

    hvordan ser det ut

--- www/prosjektrapport/pages/møtereferater.php
+++ www/prosjektrapport/pages/møtereferater.php
@@ -1,6 +1,5 @@
 <h1>Møtereferat</h1>
-<h3>Onsdag 23.10.2013</h3>
-	<p>
+	<h3>Onsdag 23.10.2013</h3>
 		<ul>
 			<li>Møttes for første gang, ble kjent.</li>
 			<li>Oprettet gruppe på facebook, mappe på drive og repository på github.</li>
@@ -19,6 +18,46 @@
 				<ul><li>Tenke hvilken tema vi ønsker å lage oppgave om. Hva vil du jobbe med? Hva vil du ikke jobbe med?</li></ul>
 			<li>Fellers interesser:</li>
 				<ul><li>Musikk, kunst og kaffe</li></ul>
-
-
 		</ul>
+
+	<h3>Torsdag 24.10.2013</h3>
+		<ul>
+			<li>Gikk kjapt gjennom forrige møtereferat</li>
+			<li>Utførte en "braindump" for å finne hvilker temaer vi interesserer oss for.</li>
+			<li>Brukte følgende demokratisk prosess for å stemme frem temaet for webprosjektet.</li>
+				<ol>
+					<li>Bli kjent med tema.</li>
+					<li>Bryte ned innhold.</li>
+					<li>Gjøre om til HTML</li>
+					<li>Gjøre interaktivt.</li>
+					<li>Ekstern til å gi vurdering.</li>
+					<li>Vurderer hva som skal gjøres.</li>
+					<li>2-6 igjen.</li>
+				</ol>
+			<li>De aller første idéene etter braindumpen så slik ut:</li>
+				<ul>
+					<li>Bahamas jul nettside</li>
+					<li>Interaktiv nettsted</li>
+					<li>Ingeniørstudent Blogg</li>
+					<li>Spill</li>
+					<li>Nettside for treningssenter</li>
+					<li>Konserter i Oslo nettside</li>
+					<li>Grevling - bli kjent med grevling</li>
+					<li>Beste ølsteder i oslo</li>
+					<li>Kebabguide</li>
+					<li>Band/artist - gir rom for kunstenriske utbroderinger: tekst, bilder, lyd, film, historiefortelling</li>
+					<li>Interaktiv nettside som lærer bort grunnprinsipper i javasript/php: forklaringspop-up, fargekoder, grupperinger osv.</li>
+					<li>Ukjent naturfenomen.</li>
+					<li>En nettside som handler om hvordan man lager en nettside.</li>
+					<li>Nettside om eyetracking, som tar hensyn til det man vet om eyetracking</li>
+					<li>Sosialt medie til skolebruk bringe lærer og elever nærmere- forum for elever, danne grupper</li>
+					<li>Nettside for bedrift - Restaurant?</li>
+					<li>Studentenes kokebok</li>
+					<li>merchselskap, utvide til nettbutikk</li>
+					<li>Fungerende sosialt nettverk, der bruker kan logge seg på og lagre informasjon om seg selv, vanskelig, fordi man trenger hele og kan ikke begynne med basics
+					nettside for skole</li>
+					<li>bilforrettning, pressentere biler</li>
+				</ul>
+		</ul>
+
+
