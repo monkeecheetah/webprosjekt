9c6a3d0065a1a745e37aa11f1d5a3430f3587cd4
kristofferosen <kristofferosen@gmail.com>
2013-11-20 15:23:32 +0100

    mekker på sluttrapport

--- www/prosjektrapport/pages/sluttrapport.php
+++ www/prosjektrapport/pages/sluttrapport.php
@@ -1,1 +1,141 @@
 <h1>Sluttrapport</h1>
+
+
+<h3>Innhold</h3>
+<a href="#innledning">1. Innledning</a>
+<ul>
+	<li><a href="#problemstilling">Problemstilling</a></li>
+	<li><a href="#mål">Mål</a></li>
+	<li><a href="#ambisjonsnivå">Ambisjonsnivå</a></li>
+</ul>
+
+
+<a href="#rammer">2. Rammer</a>
+<ul>
+	<li><a href="#teknologi">Teknologi</a></li>
+	<li><a href="#målgruppe">Målgruppe</a></li>
+	<li><a href="#arbeidsmetode">Arbeidsmetode</a></li>
+	<li><a href="#roller">Roller</a></li>
+</ul>
+
+
+<a href="#prosessen">3. Utviklingsprosess</a>
+
+
+
+<a href="#resultat">4. Resultat</a>
+
+
+<a href="#valg">5. Valg</a>
+
+
+<a href="#evaluering">6. Evaluering</a>
+
+
+<a href="#refleksjon">7. Refleksjon</a>
+
+
+<a href="#koklusjon">8. Koklusjon</a>
+
+
+<a href="#vedlegg">9. Litteraturhenvisning og andre ting(vet ikke om vi trenger denne)</a>
+
+
+
+<section id="innledning">
+	<h3>Innledning</h3>
+
+	<div id="problemstilling">
+
+	</div>
+
+	<div id="mål">
+	</div>
+
+	<div id="abisjonsnivå">
+	</div>
+</section>
+
+
+
+<section id="rammer">
+	<h3>Rammer</h3>
+	<div id ="teknologi">
+	</div>
+
+	<div id="målgruppe">
+	</div>
+
+	<div id="arbeidsmetode">
+	</div>
+
+	<div id="roller">
+	</div>
+</section>
+
+
+<section id="prosessen">
+	<h3>Utviklingsprosessen</h3>
+	Utviklingsprosess(detaljert om metode, vendepunkter, valg, ansvar, forkasting av teknologi, hvilken kaffe har vi drukket,
+
+</section>
+
+
+<section id="resultat">
+	<h3>Resultat</h3>
+	Presentasjon av resultat(Beskrivelse av oppbyggning, scrollefuksjon og  fadin og kaffevelgeren(Beskrivelse av Jquerykode), produksjon av egne bilder, beskrivelse av design, plassering av tekst, font, bakgrunsbilder, 
+</section>
+
+
+<section id="valg">
+	<h3>Valg</h3>
+	Valg (hovedsakelig anngående WCAG, begrunnelse for scrollefunksjon og sideoppbyggning samt layout.)
+
+</section>
+
+
+<section id="evaluering">
+	<h3>Evaluering</h3>
+	Evaluering(av metode, valg, teknologier, 
+
+
+</section>
+
+
+<section id="refleksjon">
+	<h3>Refleksjon</h3>
+	Refleksjon (rundt ansvar, samarbeid, ambisjonsnivå, sluttprodukt, 
+
+
+</section>
+
+
+<section id="konklusjon">
+	<h3>Konklusjon</h3>
+	Konklusjoner(hva funket/hva funket ikke, hva har vi lært, hva sitter vi igjen med
+
+
+</section>
+
+
+<section id="vedlegg">
+	<h3>Litteraturhenvisninger og begrepsforklaringer</h3>
+	Litteraturhenvisninger og begrepsforklaringer ???
+
+</section>
+
+
+
+
+
+innhold
+Innledning(mye det samme som info om gruppen)
+Forord(ide, bakgrunn, 
+Rammer(teknologier,scrum, målgruppe, mål med nettside, hvilken metode valgte vi, ambinisjonsnivåer, 
+Utviklingsprosess(detaljert om metode, vendepunkter, valg, ansvar, forkasting av teknologi, hvilken kaffe har vi drukket,
+Presentasjon av resultat(Beskrivelse av oppbyggning, scrollefuksjon og  fadin og kaffevelgeren(Beskrivelse av Jquerykode), produksjon av egne bilder, beskrivelse av design, plassering av tekst, font, bakgrunsbilder, 
+Valg (hovedsakelig anngående WCAG, begrunnelse for scrollefunksjon og sideoppbyggning samt layout.)
+Evaluering(av metode, valg, teknologier, 
+Refleksjon (rundt ansvar, samarbeid, ambisjonsnivå, sluttprodukt, 
+Konklusjoner(hva funket/hva funket ikke, 
+Litteraturhenvisninger og begrepsforklaringer ???
