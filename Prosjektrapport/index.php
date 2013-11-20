74603b23dc13a396109b827a5000d897dcaed0d1
kristofferosen <kristofferosen@gmail.com>
2013-11-20 11:42:30 +0100

    Lager prosjektrapportmappe

---
+++ Prosjektrapport/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Prosjektrapport</title>
+		<meta charset="utf-8">	
+		<link rel="stylesheet" type="text/css" title="Standard" href="style.css">
+	</head>
+
+	<body>
+		<header>
+			<h1>Prosjektrapport for gruppe 29</h1>
+		</header>
+		
+		<nav>
+			<ul>
+				<li><a href="index.php?page=1">Hoved</a></li>
+				<li><a href="index.php?page=2">Gruppedynamikk</a></li>
+				<li><a href="index.php?page=3">Forprosjektrapport</a></li>
+				<li><a href="index.php?page=4">Kravspesifikasjoner</a></li>
+				<li><a href="index.php?page=5">Milepælsplan></li>
+				<li><a href="index.php?page=6">Møtereferater</a></li>
+				<li><a href="index.php?page=7">Sluttrapport</a></li>
+				<li><a href="index.php?page=8">Vedlegg</a></li>
+			</ul>
+		</nav>
+
+		<div id="content">
+			phpincludeshit
+		</div>
+
+		<div id= "bilder">
+			<img src="images/kristoffer.jpg" alt="Kristoffer">
+			<img src="images/hege.jpg" alt="Hege">
+			<img src="images/christer.jpg" alt="Christer">
+			<img src="images/osen.jpg" alt="Kristoffer">
+		</div> 
+	</body>
+</html>
