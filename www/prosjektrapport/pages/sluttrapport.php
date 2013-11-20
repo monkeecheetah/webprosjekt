685d20558887d211ab97b2fab0eecac23a0490f1
kristofferosen <kristofferosen@gmail.com>
2013-11-20 17:15:49 +0100

    skriver og organiserer innhold i sluttrapport

--- www/prosjektrapport/pages/sluttrapport.php
+++ www/prosjektrapport/pages/sluttrapport.php
@@ -21,20 +21,22 @@
 
 <a href="#prosessen">3. Utviklingsprosess</a>
 <ul>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
+	<li><a href="#stegforsteg">Steg for steg</a></li>
+	<li><a href="#teknologiskutvikling">Teknologisk utvikling</a></li>
+	<li><a href="#tegning">Produksjon av egne bilder</a></li>
 </ul>
 
 
 
 <a href="#resultat">4. Resultat</a>
 <ul>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
-	<li><a href="">underpunkt</a></li>
+	<li><a href="#presentasjon">Presentasjon av nettside</a></li>
+	<li><a href="#scroll">Scrollefunksjon</a></li>
+	<li><a href="#kaffevelger">Kaffevelger</a></li>
+	<li><a href="#design">Design</a></li>
+	<li><a href="#bilder">Bilder</a></li>
+	<li><a href="#responsivt">Responsivt design</a></li>
+
 </ul>
 
 <a href="#valg">5. Valg</a>
@@ -136,6 +138,27 @@
 <section id="prosessen">
 	<h2>3. Utviklingsprosessen</h2>
 
+	<div id="steg">
+		<h3>Steg for steg</h3>
+		<p>Forklaring av prosessen ved hjelp av milepæler(Går vekk fra jarallax, finner substitutt,
+		 design tar form, utviklig av innhold og dramaturgi, finn eksempler i møtereferater)</p>
+	</div>
+
+	<div id="teknologiskutvikling">
+		<h3>Teknologisk utvikling</h3>
+		<p>Grundig forklaring av hvorfor ikke jarallax funket, hvordan vi løste problemet og hva vi byttet jarallax for.</p>
+	</div>
+
+	<div id="tegning">
+		<h3>Produksjon av bilder</h3>
+		<p>Hvordan var det å tegne bilder, Hege?</p>
+	</div>
+
+	<div>
+		<h3></h3>
+	</div>
+
+
 
 
 	Utviklingsprosess(detaljert om metode, vendepunkter, valg, ansvar, forkasting av teknologi, hvilken kaffe har vi drukket,
@@ -145,54 +168,72 @@
 
 <section id="resultat">
 	<h2>4. Resultat</h2>
-	<div>
+	<div id="presentasjon">
 		<h3>Presentasjon av nettsiden</h3>
 		<p>Forklare sidens grunnprinsipp og forklare nettsiden side for side</p>
 	</div>
 
-	<div>
+	<div id="scroll">
 		<h3>Scrollefunksjon</h3>
 		<p>Forklare hvordan vi har laget scrollingen</p>
 	</div>
 
-	<div>
+	<div id="kaffevelger">
 		<h3>Kaffevelger</h3>
 		<p>Forklare hvordan kaffevelgeren funker</p>
 	</div>
 
-	<div>
-		<h3>Bilder</h3>
-		
+	<div id="design">
+		<h3>Design</h3>
+		<p>Generelt om konsept(enkle, stilrene bilder, tekst over. Fortelle hvordan vi har valgt å legge tekst, fargevalg, font og tekststørrelse. </p>
 	</div>
 
-	<div>
-		<h3>Design</h3>
+	<div id="bilder">
+		<h3>Bilder</h3>
+		<p>Fortelle om bildene vi har valg, hvorfor de er valgt og hva vi har gjort med de. Litt om hege sine bilder</p>
+	</div>
+
+
+	<div id="responsivt">
+		<h3>Responsivt design</h3>
+		<p>Hva har vi gjort for at nettsiden skal funke på alle sjermer, i alle browsere</p>
 	</div>
 	Presentasjon av resultat(Beskrivelse av oppbyggning, scrollefuksjon og  fadin og kaffevelgeren(Beskrivelse av Jquerykode), produksjon av egne bilder, beskrivelse av design, plassering av tekst, font, bakgrunsbilder, 
 </section>
 
 
+
+
 <section id="valg">
 	<h2>5. Valg</h2>
+
 	Valg (hovedsakelig anngående WCAG, begrunnelse for scrollefunksjon og sideoppbyggning samt layout.)
 
 </section>
 
 
+
+
+
+
 <section id="evaluering">
 	<h2>6. Evaluering</h2>
 	Evaluering(av metode, valg, teknologier, 
-
-
 </section>
+
+
+
+
 
 
 <section id="refleksjon">
 	<h2>7. Refleksjon</h2>
 	Refleksjon (rundt ansvar, samarbeid, ambisjonsnivå, sluttprodukt, 
-
-
 </section>
+
+
+
+
 
 
 <section id="konklusjon">
