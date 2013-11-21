99b2a9e8adb0b73971c226d3e1e708cc336e22a1
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-21 11:17:56 +0100

    gjort en ny test

--- www/wwwMASTER/js/script.js
+++ www/wwwMASTER/js/script.js
@@ -2,6 +2,22 @@
 vinduBredde = $( window ).width();
 
 
+jQuery(function($){
+var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var konami_index = 0;
+$(document).keydown(function(e){
+    if(e.keyCode === konami_keys[konami_index++]){
+        if(konami_index === konami_keys.length){
+            $(document).unbind('keydown', arguments.callee);
+        	console.log("Du er dust");
+        }
+    }else{
+        konami_index = 0;
+    }
+});
+});
+
+
 $(document).ready(function(){
 	// Cache the Window object
 	
