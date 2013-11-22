8ab26e1322ca0f2c407ecf6564675819dab753a7
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-22 11:13:21 +0100

    Ryddet i script og lagt til psd av kaffebønnealfred med layers om vi vil gjøres justeringer

--- www/wwwMASTER/js/script.js
+++ www/wwwMASTER/js/script.js
@@ -1,7 +1,5 @@
 
 vinduBredde = $( window ).width();
-
-
 $(function($){
 var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var konami_index = 0;
@@ -9,7 +7,6 @@ $(document).keydown(function(e){
     if(e.keyCode === konami_keys[konami_index++]){
         if(konami_index === konami_keys.length){
             $(document).unbind('keydown', arguments.callee);
-        //	$('html').html("")
         	window.location="gameLoader.php";
         }
     }else{
@@ -17,46 +14,27 @@ $(document).keydown(function(e){
     }
 });
 });
-
-
 $(document).ready(function(){
-	// Cache the Window object
-	
 	if(vinduBredde > 900) {
 		blockFade();
-	
 		$window = $(window);
 		$('section[data-type="background"]').each(function(){
-	    	var $bgobj = $(this); // assigning the object
-	                    
+	    	var $bgobj = $(this); // assigning the object    
 	    	$(window).scroll(function() {
 	                    
 			// Scroll the background at var speed
 			// the yPos is a negative value because we're scrolling it UP!								
-				var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-			
+				var yPos = -($window.scrollTop() / $bgobj.data('speed')); 			
 			// Put together our final background position
 				var coords = '100% '+ yPos + 'px';
-
 			// Move the background
 				$bgobj.css({ backgroundPosition: coords });
 			}); // window scroll Ends
 		});	
 	}
-/*
-	introFade = $("#intro h3, #intro p,#helse h3, #helse p").fadeTo(0,0);
-		counter = 20;
-	   $("#intro h3, #intro p,#helse h3, #helse p").each(function(){
-	   		if($(this).is("h3")) {
-	   			counter = 0;
-	   		}
-	   		$(this).css('margin-left', counter+'px');
-	   		counter += 20;
-	   	});*/
-
 	   kaffeKonsum();
 	   kaffeProduksjon();
-}); // end of Document on ready
+});
 
 function kaffeKonsum(){
 	liBredde = $("#kaffeKonsum li").width();
@@ -91,155 +69,3 @@ function blockFade(){
  	});
  })
 };
-/*
-
-$(window).scroll(function () {
-   $('#intro p, #intro h3, #helse h3, #helse p').each(function () { // <---loop the divs id starts with #box 
-      if (($(this).offset().top - $(window).scrollTop()) < 200) { //<---mark the $(this).offset().top of current object
-          $(this).stop().fadeTo(1000, 0); //<----fadeOut the current obj
-      } else {
-          $(this).stop().fadeTo(1000, 1); //<----fadeIn the current obj
-      }
-   });
-});
-*/
-
-
-// Scroll listener - hvor er man - hva skal sammenliknes med hva osv
-/*
-$(window).scroll(function(){
-	scrollPos = $(window).scrollTop();
-	console.log("CurrPos "+$(window).scrollTop());
-	previous = 0;
-	current = 0;
-	next = 0;
-	toUse = 0;
-	scrollFunc();
-});
-
-
-// fadeanimasjonen
-function fadeAni(inputElements) {
-	$(inputElements).children().each(function(){
-		elX = $(inputElements).position().top;
-		console.log(this);
-		$(this).children().children().each(function(){
-			if((elX + $(this).position().top) > $(window).scrollTop()){
-				$(this).fadeTo(1000,1);
-			}
-		});
-	});
-}
-// Opprette array med alle blokkene. Er det egentlig nødvendig?
-$(window).load( function () { 
-	elementsArray = new Array;
-	$('.block').each(function(){
-		elementsArray.push(this);
-	});
-});
-
-function scrollFunc() {
-	$(elementsArray).each(function(){
-		elementPos  = $(this).parent().position().top; // elementPos er bare en liste med alle .blocks
-		toUse = $(this).parent();  // skriver ut det samme som elementPos bare selve elementene
-		old = 0;
-		if(scrollPos > $(this).parent().position().top && toUse.parent().position().top < $(this).parent().position().top) {
-			if(toUse != old) {
-				console.log(toUse);
-				fadeAni(toUse);
-				old = toUse;
-			}
-		}
-	});
-}
-*/
-/*
-$(window).scroll(function(d,h) {
-	if(vinduBredde > 900) {	
-	    introFade.each(function(i) {
-	        a = $(this).offset().top + $(this).height();
-	        b = $(window).scrollTop() + $(window).height();
-	        console.log($("#intro").children().index(this));
-	        if (a < b) {
-
-	        	$(this).fadeTo(2000,1,function(){
-	        		console.log(this);
-	        		console.log($("#intro").children().index(this));
-	        		var indeks = $("#intro").children().index(this);
-	        	});
-	        }
-		});
-	}
-}); 
-*/
-
-
-
-	// pikk = $("#kaffetilnorge p").fadeTo(0,1);
-	// 	teller = 20;
-	// 	$("kaffetilnorge p").each(function()
-	// 	{
-	// 		$(this).css('margin-left', teller + 'px');
-	// 		teller += 10;
-	// 	});
-	// $(window).scroll(function(d,h))
-	// {
-	// 	pikk.each(function(i)
-	// 	{
-	// 		pa
-	// 	}
-	// 		)
-	// }
-
-
-	// tiles = $("#kaffetilnorge h3, #kaffetilnorge p").fadeTo(0,0);
-	// 	counter = 20;
-	//    $("#kaffetilnorge h3, #kaffetilnorge p").each(function(){
-	//    		$(this).css('margin-left', counter+'px');
-	//    		counter += 20;
-	//    	});
-
-
-	// }); // end of Document on ready
-	// $(window).scroll(function(d,h) {
-	//     tiles.each(function(i) {
-	//         a = $(this).offset().top + $(this).height();
-	//         b = $(window).scrollTop() + $(window).height();
-	//         if (a < b) $(this).fadeTo(3000,1);
-	//     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
