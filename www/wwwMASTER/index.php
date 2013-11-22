90193804b1a12f4565da2ce0abfdcaf50e82e4bf
kristofferosen <kristofferosen@gmail.com>
2013-11-22 16:46:37 +0100

    footer med tabell

--- www/wwwMASTER/index.php
+++ www/wwwMASTER/index.php
@@ -411,8 +411,94 @@
 
 			<section class="blockWrap" id="bak11" data-speed="20000" data-type="background">
 				<footer>
-				<h3>Footer</h3>
-				<p>HER MÅ VI HA NOE</p>
+					<h3>Skrevet av Kristoffer Johansen, Hege Grostad, Christer Bang og Kristoffer osen</h3>
+				
+
+					<h3>Bildebank</h3>
+					<p>Alle bilder har Creative Commons-lisens og er hentet fra flickr.com sine register for Creative Commons-bilder<br>
+					Alle bildene på nettsiden er redigerte versjoner av orginalene.<br>
+					Bildene av norsk fjord, afrikansk steppelandskap og de forsjellige tilbredningsmetodene er tegnet av Hege Grostad. </p>
+					<table border="1">
+						<tr> 
+							<th>Tittel</th>
+							<th>Eier</th>
+							<th>Lenke</th>
+							<th>Filnavn</th>
+						</tr>
+
+						<tr> 
+							<td>If coffee were red</td>
+							<td>Marcello</td>
+							<td><a href="http://www.flickr.com/photos/67265930@N00/1874968239/">link</a></td>
+							<td>1874968239_5ec7cd1eef_o.jpg</td>
+						</tr>
+
+
+						<tr>
+						    <td>A Coffee bean 3</td>
+							<td>Joe Lencioni</td>
+							<td><a href="http://www.flickr.com/photos/12943844@N00/2291755823">link</a></td>
+							<td>2291755823_513c35efd6_oSjolmekket.jpg</td> 
+						</tr>
+
+						<tr> 
+							<td>teXture - Dark Grunge Burlap</td>
+							<td>photonate.com</td>
+							<td><a href="http://www.flickr.com/photos/themacguy2k/3297949680/">link</a></td>
+							<td>3297949680_f7f64e11f7_o.jpg</td>
+						</tr>
+
+						<tr> 
+							<td>Coffee Scoop</td>
+							<td>Oberazzi</td>
+							<td><a href="http://www.flickr.com/photos/42788859@N00/3367777051/">link</a></td>
+							<td>3367777051_a18a02c53d_o.jpg</td>
+						</tr>
+
+						<tr> 
+							<td>Atomium</td>
+							<td>Seb Ruiz</td>
+							<td><a href="http://www.flickr.com/photos/60906956@N00/423580043/">link</a></td>
+							<td>423580043_4d6302fb42_occaffeine3.jpg</td>
+						</tr>
+
+						<tr> 
+							<td>harimau coffee burlap</td>
+							<td>javabags</td>
+							<td><a href="http://www.flickr.com/photos/66871266@N04/6085694779/">link</a></td>
+							<td>6085694779_9a49e657f5_o.jpg</td>
+						</tr>
+
+						<tr> 
+							<td>Joie</td>
+							<td>Geoff Llerena</td>
+							<td><a href="http://www.flickr.com/photos/22678640@N03/10502752753">link</a></td>
+							<td>10502752753_673727d16b_o.jpg</td>
+						</tr>
+
+						<tr> 
+							<td>Coffe splash</td>
+							<td>Nick Olejniczak</td>
+							<td><a href="http://www.flickr.com/photos/71235231@N00/4448779926/">link</a></td>
+							<td>4448779926_b9329c1568_o.jpg</td>
+						</tr>
+
+						<tr> 
+							<td>Burlap and Lace square (3)</td>
+							<td>Lucy Portsmouth</td>
+							<td><a href="http://www.flickr.com/photos/48224373@N04/8112875566/">link</a></td>
+							<td>8112875566_653aab9940_o.jpg</td>
+						</tr>
+
+						<tr> 
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+					</table>
+
+
 				</footer>
 			</section>
 
