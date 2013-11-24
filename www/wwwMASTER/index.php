83092fe4bee0ad802761d2d05b9e7eef16dbe6cf
Kristoffer Johansen <monkeecheetah@gmail.com>
2013-11-24 13:07:25 +0100

    tabell på bunnen er fin

--- www/wwwMASTER/index.php
+++ www/wwwMASTER/index.php
@@ -425,80 +425,81 @@
 					<p>Alle bilder har Creative Commons-lisens og er hentet fra flickr.com sine register for Creative Commons-bilder<br>
 					Alle bildene på nettsiden er redigerte versjoner av orginalene.<br>
 					Bildene av norsk fjord, afrikansk steppelandskap og de forsjellige tilbredningsmetodene er tegnet av Hege Grostad. </p>
-					<table border="1">
-						<tr> 
-							<th>Tittel</th>
-							<th>Eier</th>
-							<th>Lenke</th>
-							<th>Filnavn</th>
-						</tr>
 
-						<tr> 
-							<td>If coffee were red</td>
-							<td>Marcello</td>
-							<td><a href="http://www.flickr.com/photos/67265930@N00/1874968239/">link</a></td>
-							<td>1874968239_5ec7cd1eef_o.jpg</td>
-						</tr>
+					<div class="tabell">
+						<div class="tabellRad"> 
+							<div class="celleData tHead tab1">Tittel</div>
+							<div class="celleData tHead tab2">Eier</div>
+							<div class="celleData tHead tab3">Lenke</div>
+							<div class="celleData tHead tab4">Filnavn</div>
+						</div>
+
+						<div class="tabellRad"> 
+							<div class="celleData tab1">If coffee were red</div>
+							<div class="celleData tab2">Marcello</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/67265930@N00/1874968239/">link</a></div>
+							<div class="celleData tab4">1874968239_5ec7cd1eef_o.jpg</div>
+						</div>
 
 
-						<tr>
-						    <td>A Coffee bean 3</td>
-							<td>Joe Lencioni</td>
-							<td><a href="http://www.flickr.com/photos/12943844@N00/2291755823">link</a></td>
-							<td>2291755823_513c35efd6_oSjolmekket.jpg</td> 
-						</tr>
+						<div class="tabellRad">
+						    <div class="celleData tab1">A Coffee bean 3</div>
+							<div class="celleData tab2">Joe Lencioni</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/12943844@N00/2291755823">link</a></div>
+							<div class="celleData tab4">2291755823_513c35efd6_oSjolmekket.jpg</div> 
+						</div>
 
-						<tr> 
-							<td>teXture - Dark Grunge Burlap</td>
-							<td>photonate.com</td>
-							<td><a href="http://www.flickr.com/photos/themacguy2k/3297949680/">link</a></td>
-							<td>3297949680_f7f64e11f7_o.jpg</td>
-						</tr>
+						<div class="tabellRad"> 
+							<div class="celleData tab1">teXture - Dark Grunge Burlap</div>
+							<div class="celleData tab2">photonate.com</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/themacguy2k/3297949680/">link</a></div>
+							<div class="celleData tab4">3297949680_f7f64e11f7_o.jpg</div>
+						</div>
 
-						<tr> 
-							<td>Coffee Scoop</td>
-							<td>Oberazzi</td>
-							<td><a href="http://www.flickr.com/photos/42788859@N00/3367777051/">link</a></td>
-							<td>3367777051_a18a02c53d_o.jpg</td>
-						</tr>
+						<div class="tabellRad"> 
+							<div class="celleData tab1">Coffee Scoop</div>
+							<div class="celleData tab2">Oberazzi</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/42788859@N00/3367777051/">link</a></div>
+							<div class="celleData tab4">3367777051_a18a02c53d_o.jpg</div>
+						</div>
 
-						<tr> 
-							<td>Atomium</td>
-							<td>Seb Ruiz</td>
-							<td><a href="http://www.flickr.com/photos/60906956@N00/423580043/">link</a></td>
-							<td>423580043_4d6302fb42_occaffeine3.jpg</td>
-						</tr>
+						<div class="tabellRad"> 
+							<div class="celleData tab1">Atomium</div>
+							<div class="celleData tab2">Seb Ruiz</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/60906956@N00/423580043/">link</a></div>
+							<div class="celleData tab4">423580043_4d6302fb42_occaffeine3.jpg</div>
+						</div>
 
-						<tr> 
-							<td>harimau coffee burlap</td>
-							<td>javabags</td>
-							<td><a href="http://www.flickr.com/photos/66871266@N04/6085694779/">link</a></td>
-							<td>6085694779_9a49e657f5_o.jpg</td>
-						</tr>
+						<div class="tabellRad"> 
+							<div class="celleData tab1">harimau coffee burlap</div>
+							<div class="celleData tab2">javabags</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/66871266@N04/6085694779/">link</a></div>
+							<div class="celleData tab4">6085694779_9a49e657f5_o.jpg</div>
+						</div>
 
-						<tr> 
-							<td>Joie</td>
-							<td>Geoff Llerena</td>
-							<td><a href="http://www.flickr.com/photos/22678640@N03/10502752753">link</a></td>
-							<td>10502752753_673727d16b_o.jpg</td>
-						</tr>
+						<div class="tabellRad"> 
+							<div class="celleData tab1">Joie</div>
+							<div class="celleData tab2">Geoff Llerena</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/22678640@N03/10502752753">link</a></div>
+							<div class="celleData tab4">10502752753_673727d16b_o.jpg</div>
+						</div>
 
-						<tr> 
-							<td>Coffe splash</td>
-							<td>Nick Olejniczak</td>
-							<td><a href="http://www.flickr.com/photos/71235231@N00/4448779926/">link</a></td>
-							<td>4448779926_b9329c1568_o.jpg</td>
-						</tr>
+						<div class="tabellRad"> 
+							<div class="celleData tab1">Coffe splash</div>
+							<div class="celleData tab2">Nick Olejniczak</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/71235231@N00/4448779926/">link</a></div>
+							<div class="celleData tab4">4448779926_b9329c1568_o.jpg</div>
+						</div>
 
-						<tr> 
-							<td>Burlap and Lace square (3)</td>
-							<td>Lucy Portsmouth</td>
-							<td><a href="http://www.flickr.com/photos/48224373@N04/8112875566/">link</a></td>
-							<td>8112875566_653aab9940_o.jpg</td>
-						</tr>
-					</table>
+						<div class="tabellRad"> 
+							<div class="celleData tab1">Burlap and Lace square (3)</div>
+							<div class="celleData tab2">Lucy Portsmouth</div>
+							<div class="celleData tab3"><a href="http://www.flickr.com/photos/48224373@N04/8112875566/">link</a></div>
+							<div class="celleData tab4">8112875566_653aab9940_o.jpg</div>
+						</div>
+					</div>
 
-					<p>© Copymachine Hioa 2013</p>
+					<p>© Copymachine webProsjekt.gruppe(29) 2013</p>
 				</footer>
 			</section>
 	</body>
